84d376e933a53077c18b3fb48bc5e1ae32b271be
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2019-10-29 21:25:55 +0100

    Create Helper.cs

---
+++ src/Nuclear.TestSite/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nuclear.TestSite {
+    static class Helper {
+
+        internal static String Print<TType>(this TType _this) {
+            if(_this == null) {
+                return "null";
+            }
+
+            if(_this is Type type) {
+                return String.Format(CultureInfo.InvariantCulture, "'{0}'", type.FullName);
+            }
+
+            return String.Format(CultureInfo.InvariantCulture, "'{0}'", _this);
+        }
+
+        internal static String PrintType(this Object _this) => _this != null ? _this.GetType().Print() : "null";
+    }
+}
