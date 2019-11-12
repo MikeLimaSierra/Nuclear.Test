4f22e702ef57271b4f0cd1e128d4ae057df911cc
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2019-11-12 20:21:25 +0100

    more test types

--- src/Nuclear.TestSite.Tests/TestTypes.cs
+++ src/Nuclear.TestSite.Tests/TestTypes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nuclear.TestSite;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 
@@ -15,15 +17,36 @@ namespace Ntt {
     class Two : ITwo { }
 
     class PropertyChangedClass : INotifyPropertyChanged {
+
         public event PropertyChangedEventHandler PropertyChanged;
-        private Boolean _value;
-        public Boolean Value {
-            get => _value;
+
+        private Boolean _value1;
+
+        private Boolean _value2;
+
+        public Boolean Value1 {
+            get => _value1;
             set {
-                _value = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
+                _value1 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value1"));
             }
         }
+
+        public Boolean Value2 {
+            get => _value2;
+            set {
+                _value2 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value2"));
+            }
+        }
+
+        public Boolean Value1And2 {
+            set {
+                Value1 = value;
+                Value2 = value;
+            }
+        }
+
     }
 
     internal class DummyIEquatableT : Dummy, IEquatable<DummyIEquatableT> {
@@ -58,11 +81,56 @@ namespace Ntt {
         internal Dummy(Int32 value) { Value = value; }
 
         public override String ToString() => Value.ToString();
+
+        public static implicit operator Dummy(Int32 num) => new Dummy(num);
     }
 
-    internal class DummyEqualityComparer : IEqualityComparer<Dummy> {
-        public Boolean Equals(Dummy x, Dummy y) => x.Value.Equals(y.Value);
+    internal class DummyEqualityComparer : IEqualityComparer {
+        public new Boolean Equals(Object x, Object y) {
+            if(x == null) {
+                return y == null ? true : y.Equals(x);
+            }
+
+            return y == null ? false : (x as Dummy).Value.Equals((y as Dummy).Value);
+        }
+
+        public Int32 GetHashCode(Object obj) => (obj as Dummy).Value;
+    }
+
+    internal class DummyEqualityComparerT : IEqualityComparer<Dummy> {
+        public Boolean Equals(Dummy x, Dummy y) {
+            if(x == null) {
+                return y == null ? true : y.Equals(x);
+            }
+
+            return y == null ? false : x.Value.Equals(y.Value);
+        }
+
         public Int32 GetHashCode(Dummy obj) => (obj as Dummy).Value;
+    }
+
+    internal class ThrowExceptionComparer : IEqualityComparer {
+        public new Boolean Equals(Object x, Object y) => throw new NotImplementedException();
+        public Int32 GetHashCode(Object obj) => throw new NotImplementedException();
+    }
+
+    internal class ThrowExceptionComparerT<T> : IEqualityComparer<T> {
+        public Boolean Equals(T x, T y) => throw new NotImplementedException();
+        public Int32 GetHashCode(T obj) => throw new NotImplementedException();
+    }
+
+    internal class PropertyChangedEventDataEqualityComparer : IEqualityComparer<EventData<PropertyChangedEventArgs>> {
+
+        public Boolean Equals(EventData<PropertyChangedEventArgs> x, EventData<PropertyChangedEventArgs> y) {
+            if(x == null) {
+                return y == null ? true : y.Equals(x);
+            }
+
+            return ReferenceEquals(x.Sender, y.Sender) && x.EventArgs.PropertyName.Equals(y.EventArgs.PropertyName);
+        }
+
+        public Int32 GetHashCode(EventData<PropertyChangedEventArgs> obj) => 0;
+
     }
 
 }
