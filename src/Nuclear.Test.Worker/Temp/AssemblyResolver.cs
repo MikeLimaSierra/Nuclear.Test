1c5beea3b5ff27c6613721036404646d3fda6e30
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-01-07 23:03:36 +0100

    resolver outlines

---
+++ src/Nuclear.Test.Worker/Temp/AssemblyResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Nuclear.Test.Worker.Temp {
+    abstract class AssemblyResolver : IAssemblyResolver {
+
+        #region public methods
+
+        public abstract Boolean TryResolve(ResolveEventArgs e, out FileInfo assembly);
+
+        public abstract Boolean TryResolve(ResolveEventArgs e, out IEnumerable<FileInfo> assemblies);
+
+        #endregion
+
+        #region public methods
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Test.Worker/Temp/IAssemblyResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Nuclear.Test.Worker.Temp {
+    interface IAssemblyResolver {
+
+        #region methods
+
+        Boolean TryResolve(ResolveEventArgs e, out FileInfo assembly);
+
+        Boolean TryResolve(ResolveEventArgs e, out IEnumerable<FileInfo> assemblies);
+
+        #endregion
+
+    }
+}
