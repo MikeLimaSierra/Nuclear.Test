5202bed010d9ecd2fd39950ce9fe5da99339cb72
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-03-19 17:43:04 +0100

    update scenario & key implementation

--- src/Nuclear.Test/Results/TestResultKey.cs
+++ src/Nuclear.Test/Results/TestResultKey.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Reflection;
+
+using Nuclear.Assemblies.Runtimes;
 using Nuclear.Extensions;
 
 namespace Nuclear.Test.Results {
-    internal class TestResultKey : ITestResultKey {
+    public class TestResultKey : ITestResultKey {
 
         #region statics
 
-        internal static ITestResultKey Empty => new TestResultKey(null,
-            FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
-            FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
+        public static ITestResultKey Empty => new TestResultKey(null,
+            new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
+            new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
             null, null);
 
         #endregion
@@ -18,15 +20,11 @@ namespace Nuclear.Test.Results {
 
         public String AssemblyName { get; private set; }
 
-        public FrameworkIdentifiers TargetFrameworkIdentifier { get; private set; }
-
-        public Version TargetFrameworkVersion { get; private set; }
+        public RuntimeInfo TargetRuntime { get; private set; }
 
         public ProcessorArchitecture TargetArchitecture { get; private set; }
 
-        public FrameworkIdentifiers ExecutionFrameworkIdentifier { get; private set; }
-
-        public Version ExecutionFrameworkVersion { get; private set; }
+        public RuntimeInfo ExecutionRuntime { get; private set; }
 
         public ProcessorArchitecture ExecutionArchitecture { get; private set; }
 
@@ -37,19 +35,23 @@ namespace Nuclear.Test.Results {
 
         public Boolean HasAssemblyName => !String.IsNullOrWhiteSpace(AssemblyName);
 
-        public Boolean HasTargetFramework => HasTargetFrameworkIdentifier && HasTargetFrameworkVersion && HasTargetArchitecture;
+        public Boolean HasTargetRuntime => HasTargetFrameworkIdentifier && HasTargetFrameworkVersion && HasTargetArchitecture;
 
-        public Boolean HasTargetFrameworkIdentifier => TargetFrameworkIdentifier != FrameworkIdentifiers.Unknown;
+        public Boolean HasTargetFrameworkIdentifier => HasTargetRuntime && TargetRuntime.Framework != FrameworkIdentifiers.Unsupported;
 
-        public Boolean HasTargetFrameworkVersion => TargetFrameworkVersion != null;
+        public Boolean HasTargetFrameworkVersion => HasTargetRuntime && TargetRuntime.Version != new Version();
+
+        public Boolean HasFullyQualifiedTargetRuntime => HasTargetRuntime && HasTargetFrameworkIdentifier && HasTargetFrameworkVersion;
 
         public Boolean HasTargetArchitecture => TargetArchitecture != ProcessorArchitecture.None;
 
-        public Boolean HasExecutionFramework => HasExecutionFrameworkIdentifier && HasExecutionFrameworkVersion && HasExecutionArchitecture;
+        public Boolean HasExecutionRuntime => HasExecutionFrameworkIdentifier && HasExecutionFrameworkVersion && HasExecutionArchitecture;
 
-        public Boolean HasExecutionFrameworkIdentifier => ExecutionFrameworkIdentifier != FrameworkIdentifiers.Unknown;
+        public Boolean HasExecutionFrameworkIdentifier => HasExecutionRuntime && ExecutionRuntime.Framework != FrameworkIdentifiers.Unsupported;
 
-        public Boolean HasExecutionFrameworkVersion => ExecutionFrameworkVersion != null;
+        public Boolean HasExecutionFrameworkVersion => HasExecutionRuntime && ExecutionRuntime.Version != new Version();
+
+        public Boolean HasFullyQualifiedExecutionRuntime => HasExecutionRuntime && HasExecutionFrameworkIdentifier && HasExecutionFrameworkVersion;
 
         public Boolean HasExecutionArchitecture => ExecutionArchitecture != ProcessorArchitecture.None;
 
@@ -58,7 +60,7 @@ namespace Nuclear.Test.Results {
         public Boolean HasMethodName => !String.IsNullOrWhiteSpace(MethodName);
 
 
-        public Boolean IsFullyQualified => HasAssemblyName && HasTargetFramework && HasExecutionFramework && HasFileName && HasMethodName;
+        public Boolean IsFullyQualified => HasAssemblyName && HasFullyQualifiedTargetRuntime && HasFullyQualifiedExecutionRuntime && HasFileName && HasMethodName;
 
         public TestResultKeyPrecisions Precision {
             get {
@@ -85,32 +87,26 @@ namespace Nuclear.Test.Results {
 
         internal TestResultKey(ITestScenario scenario, String fileName, String methodName)
             : this(scenario.AssemblyName,
-                  scenario.TargetFrameworkIdentifier,
-                  scenario.TargetFrameworkVersion,
+                  scenario.TargetRuntime,
                   scenario.TargetArchitecture,
-                  scenario.ExecutionFrameworkIdentifier,
-                  scenario.ExecutionFrameworkVersion,
+                  scenario.ExecutionRuntime,
                   scenario.ExecutionArchitecture,
                   fileName,
                   methodName) { }
 
         internal TestResultKey(
             String assemblyName,
-            FrameworkIdentifiers targetFrameworkIdentifier,
-            Version targetFrameworkVersion,
+            RuntimeInfo targetRuntime,
             ProcessorArchitecture targetArchitecture,
-            FrameworkIdentifiers executionFrameworkIdentifier,
-            Version executionFrameworkVersion,
+            RuntimeInfo executionRuntime,
             ProcessorArchitecture executionArchitecture,
             String fileName,
             String methodName) {
 
             AssemblyName = assemblyName;
-            TargetFrameworkIdentifier = targetFrameworkIdentifier;
-            TargetFrameworkVersion = targetFrameworkVersion;
+            TargetRuntime = targetRuntime;
             TargetArchitecture = targetArchitecture;
-            ExecutionFrameworkIdentifier = executionFrameworkIdentifier;
-            ExecutionFrameworkVersion = executionFrameworkVersion;
+            ExecutionRuntime = executionRuntime;
             ExecutionArchitecture = executionArchitecture;
             FileName = fileName;
             MethodName = methodName;
@@ -124,11 +120,9 @@ namespace Nuclear.Test.Results {
             if(match == null) { return false; }
 
             if(match.HasAssemblyName && match.AssemblyName != AssemblyName) { return false; }
-            if(match.HasTargetFrameworkIdentifier && match.TargetFrameworkIdentifier != TargetFrameworkIdentifier) { return false; }
-            if(match.HasTargetFrameworkVersion && match.TargetFrameworkVersion != TargetFrameworkVersion) { return false; }
+            if(match.HasTargetRuntime && match.TargetRuntime != TargetRuntime) { return false; }
             if(match.HasTargetArchitecture && match.TargetArchitecture != TargetArchitecture) { return false; }
-            if(match.HasExecutionFrameworkIdentifier && match.ExecutionFrameworkIdentifier != ExecutionFrameworkIdentifier) { return false; }
-            if(match.HasExecutionFrameworkVersion && match.ExecutionFrameworkVersion != ExecutionFrameworkVersion) { return false; }
+            if(match.HasExecutionRuntime && match.ExecutionRuntime != ExecutionRuntime) { return false; }
             if(match.HasExecutionArchitecture && match.ExecutionArchitecture != ExecutionArchitecture) { return false; }
             if(match.HasFileName && match.FileName != FileName) { return false; }
             if(match.HasMethodName && match.MethodName != MethodName) { return false; }
@@ -140,49 +134,58 @@ namespace Nuclear.Test.Results {
             switch(precision) {
                 case TestResultKeyPrecisions.FileName:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, TargetFrameworkVersion, TargetArchitecture,
-                        ExecutionFrameworkIdentifier, ExecutionFrameworkVersion, ExecutionArchitecture,
+                        TargetRuntime, TargetArchitecture,
+                        ExecutionRuntime, ExecutionArchitecture,
                         FileName, null);
+
                 case TestResultKeyPrecisions.ExecutionArchitecture:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, TargetFrameworkVersion, TargetArchitecture,
-                        ExecutionFrameworkIdentifier, ExecutionFrameworkVersion, ExecutionArchitecture,
+                        TargetRuntime, TargetArchitecture,
+                        ExecutionRuntime, ExecutionArchitecture,
                         null, null);
+
                 case TestResultKeyPrecisions.ExecutionFrameworkVersion:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, TargetFrameworkVersion, TargetArchitecture,
-                        ExecutionFrameworkIdentifier, ExecutionFrameworkVersion, ProcessorArchitecture.None,
+                        TargetRuntime, TargetArchitecture,
+                        ExecutionRuntime, ProcessorArchitecture.None,
                         null, null);
+
                 case TestResultKeyPrecisions.ExecutionFrameworkIdentifier:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, TargetFrameworkVersion, TargetArchitecture,
-                        ExecutionFrameworkIdentifier, null, ProcessorArchitecture.None,
+                        TargetRuntime, TargetArchitecture,
+                        new RuntimeInfo(ExecutionRuntime.Framework, new Version()), ProcessorArchitecture.None,
                         null, null);
+
                 case TestResultKeyPrecisions.TargetArchitecture:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, TargetFrameworkVersion, TargetArchitecture,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
+                        TargetRuntime, TargetArchitecture,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
                         null, null);
+
                 case TestResultKeyPrecisions.TargetFrameworkVersion:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, TargetFrameworkVersion, ProcessorArchitecture.None,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
+                        TargetRuntime, ProcessorArchitecture.None,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
                         null, null);
+
                 case TestResultKeyPrecisions.TargetFrameworkIdentifier:
                     return new TestResultKey(AssemblyName,
-                        TargetFrameworkIdentifier, null, ProcessorArchitecture.None,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
+                        new RuntimeInfo(TargetRuntime.Framework, new Version()), ProcessorArchitecture.None,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
                         null, null);
+
                 case TestResultKeyPrecisions.AssemblyName:
                     return new TestResultKey(AssemblyName,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
                         null, null);
+
                 case TestResultKeyPrecisions.None:
                     return new TestResultKey(null,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
-                        FrameworkIdentifiers.Unknown, null, ProcessorArchitecture.None,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
+                        new RuntimeInfo(FrameworkIdentifiers.Unsupported, new Version()), ProcessorArchitecture.None,
                         null, null);
+
                 default:
                     break;
             }
@@ -217,19 +220,19 @@ namespace Nuclear.Test.Results {
                 Int32 result = AssemblyName.CompareTo(other.AssemblyName);
                 if(result != 0) { return result; }
 
-                result = TargetFrameworkIdentifier.CompareTo(other.TargetFrameworkIdentifier);
+                result = TargetRuntime.Framework.CompareTo(other.TargetRuntime.Framework);
                 if(result != 0) { return result; }
 
-                result = TargetFrameworkVersion.CompareTo(other.TargetFrameworkVersion);
+                result = TargetRuntime.Version.CompareTo(other.TargetRuntime.Version);
                 if(result != 0) { return result; }
 
                 result = TargetArchitecture.CompareTo(other.TargetArchitecture);
                 if(result != 0) { return result; }
 
-                result = ExecutionFrameworkIdentifier.CompareTo(other.ExecutionFrameworkIdentifier);
+                result = ExecutionRuntime.Framework.CompareTo(other.ExecutionRuntime.Framework);
                 if(result != 0) { return result; }
 
-                result = ExecutionFrameworkVersion.CompareTo(other.ExecutionFrameworkVersion);
+                result = ExecutionRuntime.Version.CompareTo(other.ExecutionRuntime.Version);
                 if(result != 0) { return result; }
 
                 result = ExecutionArchitecture.CompareTo(other.ExecutionArchitecture);
@@ -249,11 +252,9 @@ namespace Nuclear.Test.Results {
             Int32 hashCode = 0;
 
             hashCode += AssemblyName != null ? AssemblyName.GetHashCode() : 0;
-            hashCode -= TargetFrameworkIdentifier.GetHashCode();
-            hashCode += TargetFrameworkVersion != null ? TargetFrameworkVersion.GetHashCode() : 0;
-            hashCode -= TargetArchitecture.GetHashCode();
-            hashCode += ExecutionFrameworkIdentifier.GetHashCode();
-            hashCode -= ExecutionFrameworkVersion != null ? ExecutionFrameworkVersion.GetHashCode() : 0;
+            hashCode -= HasTargetRuntime ? TargetRuntime.GetHashCode() : 0;
+            hashCode += TargetArchitecture.GetHashCode();
+            hashCode -= HasExecutionRuntime ? ExecutionRuntime.GetHashCode() : 0;
             hashCode += ExecutionArchitecture.GetHashCode();
             hashCode -= FileName != null ? FileName.GetHashCode() : 0;
             hashCode += MethodName != null ? MethodName.GetHashCode() : 0;
@@ -262,7 +263,7 @@ namespace Nuclear.Test.Results {
         }
 
         public override String ToString() =>
-            $"[{AssemblyName.Format()};{TargetFrameworkIdentifier.Format()};{TargetFrameworkVersion.Format()};{TargetArchitecture.Format()};{ExecutionFrameworkIdentifier.Format()};{ExecutionFrameworkVersion.Format()};{ExecutionArchitecture.Format()};{FileName.Format()};{MethodName.Format()}]";
+            $"[{AssemblyName.Format()};{TargetRuntime.Format()};{TargetArchitecture.Format()};{ExecutionRuntime.Format()};{ExecutionArchitecture.Format()};{FileName.Format()};{MethodName.Format()}]";
 
         #endregion
 
