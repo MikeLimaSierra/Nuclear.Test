8cb8d94446eb5de9049e6bf4b2a69c9dd221dbc4
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-03-19 17:51:22 +0100

    update debug tests

--- src/Nuclear.Test.Tests/Temp/IgnoredTestMethods.cs
+++ src/Nuclear.Test.Tests/Temp/IgnoredTestMethods.cs
@@ -1,5 +1,4 @@
-﻿
-using TestMethodX = Nuclear.TestSite.TestMethodAttribute;
+﻿using TestMethodX = Nuclear.TestSite.TestMethodAttribute;
 using TestX = Nuclear.TestSite.Test;
 
 namespace Nuclear.Test {
