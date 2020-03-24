1ed250458f20d8f9b4c01edfe835d679c4d07e83
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-03-24 18:35:51 +0100

    Update TestEntry.cs

--- src/Nuclear.Test/Results/TestEntry.cs
+++ src/Nuclear.Test/Results/TestEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using Nuclear.Exceptions;
 
 namespace Nuclear.Test.Results {
@@ -17,15 +18,38 @@ namespace Nuclear.Test.Results {
         #region ctors
 
         internal TestEntry(EntryTypes type, String instruction, String message) {
-            if(type != EntryTypes.Note && type != EntryTypes.Error) {
+            if(type == EntryTypes.ResultOk || type == EntryTypes.ResultFail) {
                 Throw.If.String.IsNullOrWhiteSpace(instruction, nameof(instruction));
+                Instruction = instruction;
             }
 
             Throw.If.String.IsNullOrWhiteSpace(message, nameof(message));
 
             EntryType = type;
-            Instruction = instruction;
             Message = message;
+        }
+
+        #endregion
+
+        #region methods
+
+        internal static ITestEntry FromResult(Boolean result, String instruction, String message) {
+            Throw.If.String.IsNullOrWhiteSpace(instruction, nameof(instruction));
+            Throw.If.String.IsNullOrWhiteSpace(message, nameof(message));
+
+            return new TestEntry(result ? EntryTypes.ResultOk : EntryTypes.ResultFail, instruction, message);
+        }
+
+        internal static ITestEntry FromNote(String message) {
+            Throw.If.String.IsNullOrWhiteSpace(message, nameof(message));
+
+            return new TestEntry(EntryTypes.Note, null, message);
+        }
+
+        internal static ITestEntry FromError(String message) {
+            Throw.If.String.IsNullOrWhiteSpace(message, nameof(message));
+
+            return new TestEntry(EntryTypes.Error, null, message);
         }
 
         #endregion
