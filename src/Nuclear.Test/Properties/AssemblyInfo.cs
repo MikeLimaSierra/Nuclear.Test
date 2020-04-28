1ce93e5b5284114e47c68529868eec66c124c4be
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-28 13:11:04 +0200

    Update AssemblyInfo.cs

--- src/Nuclear.Test/Properties/AssemblyInfo.cs
+++ src/Nuclear.Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Nuclear.TestSite.uTests")]
+[assembly: InternalsVisibleTo("Nuclear.TestSite.Dummies")]
 [assembly: InternalsVisibleTo("Nuclear.Test.Tests")]
