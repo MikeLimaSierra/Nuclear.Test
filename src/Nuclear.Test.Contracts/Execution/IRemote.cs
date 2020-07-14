a9c7100deab5b0ead693b2db93aca223443e9d32
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-14 17:00:31 +0200

    add new remote interface

--- src/Nuclear.Test.Contracts/Execution/IRemote.cs
+++ src/Nuclear.Test.Contracts/Execution/IRemote.cs
@@ -1,30 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Nuclear.Test.Execution {
 
     /// <summary>
-    /// Defines the base functionality of remote control for test clients.
+    /// Defines the base functionality of a remote control for test clients.
     /// </summary>
     public interface IRemote {
 
         #region events
 
         /// <summary>
+        /// Is raised when the client connects.
+        /// </summary>
+        event EventHandler ClientConnected;
+
+        /// <summary>
+        /// Is raised when the connection to the client was lost.
+        /// </summary>
+        event EventHandler ConnectionLost;
+
+        /// <summary>
         /// Is raised when raw test data is received from the attached test client.
         /// </summary>
-        public event ResultsReceivedEventHandler ResultsReceived;
+        event ResultsReceivedEventHandler ResultsReceived;
 
         /// <summary>
         /// Is raised when test results are deserialized and added to the results.
         /// </summary>
-        public event ResultsAvailableEventHandler ResultsAvailable;
+        event ResultsAvailableEventHandler ResultsAvailable;
 
         /// <summary>
-        /// Is raised when all test results have been sent.
+        /// Is raised when the client has finished processing and all results have been transfered.
         /// </summary>
-        public event EventHandler Finished;
+        event EventHandler RemotingFinished;
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Commands the client to execute its task.
+        /// </summary>
+        void Execute();
 
         #endregion
 
