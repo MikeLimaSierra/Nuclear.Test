5b5a97902eadfacbab1ad7f24cb04d7e18b4555f
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-20 20:41:02 +0200

    Update IMessage.cs

--- src/Nuclear.Test.Contracts/Link/IMessage.cs
+++ src/Nuclear.Test.Contracts/Link/IMessage.cs
@@ -2,9 +2,14 @@
 
 namespace Nuclear.Test.Link {
 
+    /// <summary>
+    /// Defines a message that can be transmitted between <see cref="ILink"/> instances.
+    /// </summary>
     public interface IMessage {
 
         #region properties
+
+        Int32 Index { get; internal set; }
 
         String Command { get; }
 
