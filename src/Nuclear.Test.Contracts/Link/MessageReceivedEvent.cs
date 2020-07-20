47b7b5a25bf5e0c0d9557ae65862794c4e76a408
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-20 21:06:06 +0200

    message received event

---
+++ src/Nuclear.Test.Contracts/Link/MessageReceivedEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Nuclear.Exceptions;
+
+namespace Nuclear.Test.Link {
+
+    /// <summary>
+    /// An event handler delegate to handle <see cref="MessageReceivedEventArgs"/>.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The arguments.</param>
+    public delegate void MessageReceivedEventHandler(Object sender, MessageReceivedEventArgs e);
+
+    /// <summary>
+    /// An event holding a message that was received.
+    /// </summary>
+    public class MessageReceivedEventArgs : EventArgs {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the message that was received.
+        /// </summary>
+        public IMessage Message { get; private set; }
+
+        #endregion
+
+        #region ctors
+
+        /// <summary>
+        /// Creates an instance of <see cref="MessageReceivedEventArgs"/>.
+        /// </summary>
+        /// <param name="message">The message that was received.</param>
+        public MessageReceivedEventArgs(IMessage message) {
+            Throw.If.Object.IsNull(message, nameof(message));
+
+            Message = message;
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Test.Tests/Link/MessageReceivedEventArgs_uTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Nuclear.TestSite;
+
+using TestX = Nuclear.TestSite.Test;
+
+namespace Nuclear.Test.Link {
+    class MessageReceivedEventArgs_uTests {
+
+        [TestMethod]
+        void Implementation() {
+
+            TestX.If.Type.IsSubClass<MessageReceivedEventArgs, EventArgs>();
+
+        }
+
+        [TestMethod]
+        void Ctor() {
+
+            MessageReceivedEventArgs e = null;
+            IMessage msg = new Message(42, "command", new Byte[] { 0x01, 0x02, 0x03, 0x04, 0x06 });
+
+            TestX.If.Action.ThrowsException(() => e = new MessageReceivedEventArgs(null), out ArgumentException ex);
+            TestX.If.Value.IsEqual(ex.ParamName, "message");
+
+            TestX.IfNot.Action.ThrowsException(() => e = new MessageReceivedEventArgs(msg), out Exception ex1);
+            TestX.If.Reference.IsEqual(e.Message, msg);
+            TestX.If.Value.IsEqual(e.Message.Index, 42);
+            TestX.If.Value.IsEqual(e.Message.Command, "command");
+            TestX.If.Enumerable.MatchesExactly(e.Message.Payload, new Byte[] { 0x01, 0x02, 0x03, 0x04, 0x06 });
+
+        }
+
+    }
+}
