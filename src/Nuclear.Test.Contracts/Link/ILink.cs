43246234291e3d0c5b59a779d2b2dc728cb7e1be
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-20 19:01:56 +0200

    add IMessage

--- src/Nuclear.Test.Contracts/Link/ILink.cs
+++ src/Nuclear.Test.Contracts/Link/ILink.cs
@@ -10,7 +10,13 @@ namespace Nuclear.Test.Link {
 
         void Stop();
 
-        Boolean Send<T>(String command, T data);
+        Boolean Send(IMessage message);
+
+        Boolean Send(String command, Boolean data);
+
+        Boolean Send(String command, Int32 data);
+
+        Boolean Send(String command, String data);
 
         #endregion
 
---
+++ src/Nuclear.Test.Contracts/Link/IMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nuclear.Test.Link {
+
+    public interface IMessage {
+
+        #region properties
+
+        String Command { get; }
+
+        Byte[] Payload { get; }
+
+        #endregion
+
+    }
+}
