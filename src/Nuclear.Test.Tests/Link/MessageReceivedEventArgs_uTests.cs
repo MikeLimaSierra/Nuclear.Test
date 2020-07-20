0559803b58158ce5127d8085deb51c632057c638
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-20 22:21:16 +0200

    Update MessageReceivedEventArgs_uTests.cs

--- src/Nuclear.Test.Tests/Link/MessageReceivedEventArgs_uTests.cs
+++ src/Nuclear.Test.Tests/Link/MessageReceivedEventArgs_uTests.cs
@@ -18,14 +18,13 @@ namespace Nuclear.Test.Link {
         void Ctor() {
 
             MessageReceivedEventArgs e = null;
-            IMessage msg = new Message(42, "command", new Byte[] { 0x01, 0x02, 0x03, 0x04, 0x06 });
+            IMessage msg = new Message("command", new Byte[] { 0x01, 0x02, 0x03, 0x04, 0x06 });
 
             TestX.If.Action.ThrowsException(() => e = new MessageReceivedEventArgs(null), out ArgumentException ex);
             TestX.If.Value.IsEqual(ex.ParamName, "message");
 
             TestX.IfNot.Action.ThrowsException(() => e = new MessageReceivedEventArgs(msg), out Exception ex1);
             TestX.If.Reference.IsEqual(e.Message, msg);
-            TestX.If.Value.IsEqual(e.Message.Index, 42);
             TestX.If.Value.IsEqual(e.Message.Command, "command");
             TestX.If.Enumerable.MatchesExactly(e.Message.Payload, new Byte[] { 0x01, 0x02, 0x03, 0x04, 0x06 });
 
