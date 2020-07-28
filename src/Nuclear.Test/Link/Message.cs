01d1920c27dd471f0c5a3fffd20d1dd5e018589a
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-28 15:32:53 +0200

    Update Message.cs

--- src/Nuclear.Test/Link/Message.cs
+++ src/Nuclear.Test/Link/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using Nuclear.Exceptions;
 using Nuclear.Extensions;
@@ -15,34 +16,53 @@ namespace Nuclear.Test.Link {
         /// <summary>
         /// Gets the command.
         /// </summary>
-        public String Command { get; internal set; }
+        public String Command { get; }
 
         /// <summary>
         /// Gets the payload.
         /// </summary>
-        public Byte[] Payload { get; internal set; }
+        public MemoryStream Payload { get; private set; }
 
         #endregion
 
         #region ctors
 
-        private Message() { }
+        private Message(String command, MemoryStream payload) {
+            Throw.If.String.IsNullOrWhiteSpace(command, nameof(command));
+            Throw.If.Object.IsNull(payload, nameof(payload));
+
+            Command = command;
+            Payload = payload;
+        }
 
         #endregion
 
         #region methods
 
-        public Int32 CompareTo(Object obj) => throw new NotImplementedException();
-
-        public Int32 CompareTo(IMessage other) => throw new NotImplementedException();
-
-        public override Boolean Equals(Object obj) => obj != null && obj is IMessage other && Equals(other);
-
-        public Boolean Equals(IMessage other) => other != null && other.Command == Command && other.Payload == Payload;
-
-        public override Int32 GetHashCode() => Command.GetHashCode() ^ Payload.GetHashCode();
-
         public override String ToString() => $"{typeof(Message).Format()} ({Command.Format()} => {Payload.Length.Format()} Bytes)";
+
+        #endregion
+
+        #region IDisposable
+
+        private Boolean _disposedValue;
+
+        protected virtual void Dispose(Boolean disposing) {
+            if(!_disposedValue) {
+                if(disposing) {
+                    Payload?.Dispose();
+                    Payload = null;
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
 
         #endregion
 
@@ -54,11 +74,260 @@ namespace Nuclear.Test.Link {
         /// <param name="command">The message command.</param>
         /// <param name="payload">The message payload.</param>
         /// <returns>The new message object.</returns>
-        public static IMessage FromData(String command, Byte[] payload) {
-            Throw.If.Object.IsNull(command, nameof(command));
+        public static IMessage FromData(String command, Boolean payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Byte payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, SByte payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Char payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Int16 payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, UInt16 payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Int32 payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, UInt32 payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Int64 payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, UInt64 payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Single payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Double payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Decimal payload) {
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, String payload) {
             Throw.If.Object.IsNull(payload, nameof(payload));
 
-            return new Message() { Command = command, Payload = payload };
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Byte[] payload) {
+            Throw.If.Object.IsNull(payload, nameof(payload));
+
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Message"/>.
+        /// </summary>
+        /// <param name="command">The message command.</param>
+        /// <param name="payload">The message payload.</param>
+        /// <returns>The new message object.</returns>
+        public static IMessage FromData(String command, Char[] payload) {
+            Throw.If.Object.IsNull(payload, nameof(payload));
+
+            MemoryStream ms = new MemoryStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ms)) {
+                bw.Write(payload);
+            }
+
+            return new Message(command, ms);
         }
 
         #endregion
