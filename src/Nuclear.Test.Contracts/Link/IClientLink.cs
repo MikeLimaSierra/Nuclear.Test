71b756c46a4dd069cdec58234dbb9e3b8d98c6f6
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-29 11:45:26 +0200

    Update IClientLink.cs

--- src/Nuclear.Test.Contracts/Link/IClientLink.cs
+++ src/Nuclear.Test.Contracts/Link/IClientLink.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 
 namespace Nuclear.Test.Link {
+
+    /// <summary>
+    /// Defines a clientside communication link.
+    /// </summary>
     public interface IClientLink : ILink {
 
         #region events
 
+        /// <summary>
+        /// Is raised when the server has made connection to the output channel.
+        /// </summary>
         event EventHandler ServerConnected;
 
         #endregion
