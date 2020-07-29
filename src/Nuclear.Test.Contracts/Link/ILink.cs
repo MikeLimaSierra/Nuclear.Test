89c0c262ef19adc0adccae5b547a7d4510d98dab
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-07-29 11:45:20 +0200

    Update ILink.cs

--- src/Nuclear.Test.Contracts/Link/ILink.cs
+++ src/Nuclear.Test.Contracts/Link/ILink.cs
@@ -2,22 +2,54 @@
 
 namespace Nuclear.Test.Link {
 
+    /// <summary>
+    /// Defines the basic communication link.
+    /// </summary>
     public interface ILink {
 
         #region events
 
+        /// <summary>
+        /// Is raised when an <see cref="IMessage"/> is received through the input channel.
+        /// </summary>
         event MessageReceivedEventHandler MessageReceived;
 
+        /// <summary>
+        /// Is raised when connected to the output channel of another <see cref="ILink"/>.
+        /// </summary>
         event EventHandler Connected;
 
         #endregion
 
         #region methods
 
+        /// <summary>
+        /// Starts the output channel.
+        /// </summary>
+        /// <returns>True if successful.</returns>
         Boolean Start();
 
+        /// <summary>
+        /// Connects to another <see cref="ILink"/>.
+        /// </summary>
+        /// <returns>True if successful.</returns>
+        Boolean Connect();
+
+        /// <summary>
+        /// Disconnects from the input channel.
+        /// </summary>
+        void Disconnect();
+
+        /// <summary>
+        /// Disconnects from the input channel and stops the output channel.
+        /// </summary>
         void Stop();
 
+        /// <summary>
+        /// Sends an <see cref="IMessage"/> through the output channel.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         Boolean Send(IMessage message);
 
         #endregion
