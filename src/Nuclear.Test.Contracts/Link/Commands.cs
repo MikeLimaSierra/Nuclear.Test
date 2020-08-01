5ae4af653c9b634ada3b759d6d2bebcd1948af96
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-08-01 16:49:11 +0200

    Create Commands.cs

---
+++ src/Nuclear.Test.Contracts/Link/Commands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nuclear.Test.Link {
+
+    /// <summary>
+    /// Presents a predefined set of available commands for <see cref="IMessage"/>
+    /// </summary>
+    public static class Commands {
+
+        /// <summary>
+        /// The message payload contains serialized test results.
+        /// </summary>
+        public const String Results = "Test.Results";
+
+        /// <summary>
+        /// The client has finished execution and all results have been transmitted.
+        /// </summary>
+        public const String Finished = "Test.Finished";
+
+    }
+
+}
