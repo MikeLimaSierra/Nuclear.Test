d016692e3b6d64a3527c6c6be3aab3a6066a00b4
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-08-01 16:49:34 +0200

    Create IClient.cs

---
+++ src/Nuclear.Test.Contracts/Execution/IClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Nuclear.Test.Execution {
+
+    /// <summary>
+    /// Defines the base functionality of a remote controlled test client.
+    /// </summary>
+    public interface IClient {
+
+        #region events
+
+        /// <summary>
+        /// Is raised when the remote connects.
+        /// </summary>
+        event EventHandler RemoteConnected;
+
+        /// <summary>
+        /// Is raised when the connection to the remote was lost.
+        /// </summary>
+        event EventHandler ConnectionLost;
+
+        /// <summary>
+        /// Is raised when the client has finished processing and all results have been transfered.
+        /// </summary>
+        event EventHandler ExecutionFinished;
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Commands the client to execute its task.
+        /// </summary>
+        void Execute();
+
+        #endregion
+
+    }
+}
