b7cc87e8c02871d3a86b2aafa6dcf5c5a97a3b9d
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-08-01 16:50:00 +0200

    Create Client.cs

---
+++ src/Nuclear.Test/Execution/Client.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using Nuclear.Exceptions;
+using Nuclear.Test.Link;
+
+namespace Nuclear.Test.Execution {
+
+    /// <summary>
+    /// Implements the base functionality of a remote controlled test client.
+    /// </summary>
+    public abstract class Client : IClient {
+
+        #region events
+
+        /// <summary>
+        /// Is raised when the remote connects.
+        /// </summary>
+        public event EventHandler RemoteConnected;
+
+        /// <summary>
+        /// Is raised when the connection to the remote was lost.
+        /// </summary>
+        public event EventHandler ConnectionLost;
+
+        /// <summary>
+        /// Is raised when the client has finished processing and all results have been transfered.
+        /// </summary>
+        public event EventHandler ExecutionFinished;
+
+        #endregion
+
+        #region fields
+
+        private readonly IClientLink _link;
+
+        #endregion
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Client"/>.
+        /// </summary>
+        /// <param name="link">The link object used to communicate with the remote.</param>
+        public Client(IClientLink link) {
+            Throw.If.Object.IsNull(link, nameof(link));
+
+            _link = link;
+            _link.ServerConnected += OnServerConnected;
+            _link.Start();
+            _link.Connect();
+        }
+
+        #endregion
+
+        #region event handlers
+
+        private void OnServerConnected(Object sender, EventArgs e) {
+            _link.ServerConnected -= OnServerConnected;
+            RaiseRemoteConnected();
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Commands the client to execute its task.
+        /// </summary>
+        public void Execute() => throw new NotImplementedException();
+
+        #endregion
+
+        #region protected methods
+
+        /// <summary>
+        /// Raises the event <see cref="RemoteConnected"/>.
+        /// </summary>
+        protected internal void RaiseRemoteConnected() => RemoteConnected?.Invoke(this, new EventArgs());
+
+        /// <summary>
+        /// Raises the event <see cref="ConnectionLost"/>.
+        /// </summary>
+        protected internal void RaiseConnectionLost() => ConnectionLost?.Invoke(this, new EventArgs());
+
+        /// <summary>
+        /// Raises the event <see cref="ExecutionFinished"/>.
+        /// </summary>
+        protected internal void RaiseExecutionFinished() => ExecutionFinished?.Invoke(this, new EventArgs());
+
+        #endregion
+
+    }
+}
