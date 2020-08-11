501fd539fc10fc9c464a31ede6e4d86c5546b607
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-08-11 22:01:32 +0200

    Create IRemoteConfiguration.cs

---
+++ src/Nuclear.Test.Contracts/Configurations/IRemoteConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Nuclear.Test.Execution;
+
+namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Defines configuration values for an <see cref="IRemote"/>.
+    /// </summary>
+    public interface IRemoteConfiguration {
+
+        /// <summary>
+        /// Gets or sets if client processes should be started in visible windows.
+        /// </summary>
+        Boolean ShowClients { get; set; }
+
+    }
+}
