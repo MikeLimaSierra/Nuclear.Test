1026c5f196a67d765a6169f3f4549d9bfc759ccb
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-18 11:13:36 +0200

    Create IClientConfiguration.cs

---
+++ src/Nuclear.Test.Contracts/Configurations/IClientConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Nuclear.Test.Execution;
+
+namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Defines configuration values for an <see cref="IClient"/>.
+    /// </summary>
+    public interface IClientConfiguration {
+
+        /// <summary>
+        /// Gets or sets if client window will remain open after test execution.
+        ///     If set to true, the client will automatically shut down.
+        ///     If set to false, the client will will wait for console input.
+        /// </summary>
+        public Boolean AutoShutdown { get; set; }
+
+    }
+}
