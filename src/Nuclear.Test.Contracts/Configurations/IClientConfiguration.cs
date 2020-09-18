c2a21d681ccae59332e58a3f797e702053808b2c
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-18 12:30:31 +0200

    Update IClientConfiguration.cs

--- src/Nuclear.Test.Contracts/Configurations/IClientConfiguration.cs
+++ src/Nuclear.Test.Contracts/Configurations/IClientConfiguration.cs
@@ -9,12 +9,25 @@ namespace Nuclear.Test.Configurations {
     /// </summary>
     public interface IClientConfiguration {
 
+        #region constants
+
+        /// <summary>
+        /// Configuration values for <see cref="AutoShutdown"/>.
+        /// </summary>
+        public String AUTO_SHUTDOWN { get; }
+
+        #endregion
+
+        #region properties
+
         /// <summary>
         /// Gets or sets if client window will remain open after test execution.
         ///     If set to true, the client will automatically shut down.
         ///     If set to false, the client will will wait for console input.
         /// </summary>
         public Boolean AutoShutdown { get; set; }
+
+        #endregion
 
     }
 }
