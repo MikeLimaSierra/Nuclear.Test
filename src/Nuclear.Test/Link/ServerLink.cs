ee4cf5aa04838a6ee81913c261b2dbdb5619dec4
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-18 13:06:08 +0200

    Create ServerLink.cs

---
+++ src/Nuclear.Test/Link/ServerLink.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Nuclear.Test.Link {
+
+    /// <summary>
+    /// Implements the server side communication link.
+    /// </summary>
+    public class ServerLink : Link, IServerLink {
+
+        #region events
+
+        /// <summary>
+        /// Is raised when the client has made connection to the output channel.
+        /// </summary>
+        public event EventHandler ClientConnected;
+
+        /// <summary>
+        /// Is raised when connected to the output channel of an <see cref="IClientLink"/>.
+        /// </summary>
+        public event EventHandler ConnectedToClient;
+
+        #endregion
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ServerLink"/>.
+        /// </summary>
+        /// <param name="pipeID">The ID that both inbound and outbound pipe IDs are based on.</param>
+        public ServerLink(String pipeID) : base(pipeID) { }
+
+        #endregion
+
+        #region ILink methods
+
+        /// <summary>
+        /// Starts the output channel.
+        /// </summary>
+        /// <returns>True if successful.</returns>
+        public override Boolean Start() {
+            if(base.Start()) {
+                RaiseClientConnected();
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Connects to another <see cref="ILink"/>.
+        /// </summary>
+        /// <returns>True if successful.</returns>
+        public override Boolean Connect() {
+            if(base.Connect()) {
+                RaiseConnectedToClient();
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region protected methods
+
+        /// <summary>
+        /// Raises the event <see cref="ClientConnected"/>.
+        /// </summary>
+        protected internal void RaiseClientConnected() => ClientConnected?.Invoke(this, new EventArgs());
+
+        /// <summary>
+        /// Raises the event <see cref="ConnectedToClient"/>.
+        /// </summary>
+        protected internal void RaiseConnectedToClient() => ConnectedToClient?.Invoke(this, new EventArgs());
+
+        #endregion
+
+    }
+}
