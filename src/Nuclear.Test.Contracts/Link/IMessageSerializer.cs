08d210236d9dbda39f0413fc8e39348283494626
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-21 22:27:28 +0200

    Create IMessageSerializer.cs

---
+++ src/Nuclear.Test.Contracts/Link/IMessageSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nuclear.Test.Link {
+
+    /// <summary>
+    /// Defines a message handler that has capabilities of serializing and deserializing.
+    /// </summary>
+    public interface IMessageSerializer {
+
+        /// <summary>
+        /// Serializes an <see cref="IMessage"/> to a byte array.
+        /// </summary>
+        /// <param name="message">The <see cref="IMessage"/> that is serialized.</param>
+        /// <returns>The byte array.</returns>
+        Byte[] Serialize(IMessage message);
+
+        /// <summary>
+        /// Deserializes a byte array into an <see cref="IMessage"/>.
+        /// </summary>
+        /// <param name="data">The byte array that is deserialized.</param>
+        /// <returns>The <see cref="IMessage"/>.</returns>
+        IMessage Deserialize(Byte[] data);
+
+    }
+}
