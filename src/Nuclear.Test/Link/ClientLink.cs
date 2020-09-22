cdb125f63d315595ce622dc33b0ce9f4aafbc6ac
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-22 11:07:18 +0200

    Update ClientLink.cs

--- src/Nuclear.Test/Link/ClientLink.cs
+++ src/Nuclear.Test/Link/ClientLink.cs
@@ -21,6 +21,20 @@ namespace Nuclear.Test.Link {
 
         #endregion
 
+        #region properties
+
+        /// <summary>
+        /// Gets the ID of the outbound pipe.
+        /// </summary>
+        public override String PipeIDOut => $"{PipeID}-Client";
+
+        /// <summary>
+        /// Gets the ID of the inbound pipe.
+        /// </summary>
+        public override String PipeIDIn => $"{PipeID}-Server";
+
+        #endregion
+
         #region ctors
 
         /// <summary>
