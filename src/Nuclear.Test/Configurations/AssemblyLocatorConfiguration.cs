52934fabebfbf535a33459c753ef0def0641bbca
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-23 14:03:28 +0200

    get rid of old configuration types

--- src/Nuclear.Test/Configurations/AssemblyLocatorConfiguration.cs
+++ src/Nuclear.Test/Configurations/AssemblyLocatorConfiguration.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
 
-namespace Nuclear.Test.Configurations {
+//namespace Nuclear.Test.Configurations {
 
-    /// <summary>
-    /// Holds configuration values for assembly location.
-    /// </summary>
-    public class AssemblyLocatorConfiguration {
+//    /// <summary>
+//    /// Holds configuration values for assembly location.
+//    /// </summary>
+//    public class AssemblyLocatorConfiguration {
 
-        #region constants
+//        #region constants
 
-        /// <summary>
-        /// Gets the command value for SearchDir.
-        /// </summary>
-        public const String COM_SEARCH_DIR = "Locator.SearchDir";
+//        /// <summary>
+//        /// Gets the command value for SearchDir.
+//        /// </summary>
+//        public const String COM_SEARCH_DIR = "Locator.SearchDir";
 
-        /// <summary>
-        /// Gets the command value for SearchDepth.
-        /// </summary>
-        public const String COM_SEARCH_DEPTH = "Locator.SearchDepth";
+//        /// <summary>
+//        /// Gets the command value for SearchDepth.
+//        /// </summary>
+//        public const String COM_SEARCH_DEPTH = "Locator.SearchDepth";
 
-        /// <summary>
-        /// Gets the command value for SearchPattern.
-        /// </summary>
-        public const String COM_SEARCH_PATTERN = "Locator.SearchPattern";
+//        /// <summary>
+//        /// Gets the command value for SearchPattern.
+//        /// </summary>
+//        public const String COM_SEARCH_PATTERN = "Locator.SearchPattern";
 
-        /// <summary>
-        /// Gets the command value for IgnoredDirNames.
-        /// </summary>
-        public const String COM_IGNORED_DIR_NAMES = "Locator.IgnoredDirNames";
+//        /// <summary>
+//        /// Gets the command value for IgnoredDirNames.
+//        /// </summary>
+//        public const String COM_IGNORED_DIR_NAMES = "Locator.IgnoredDirNames";
 
-        #endregion
+//        #endregion
 
-        #region properties
+//        #region properties
 
-        /// <summary>
-        /// Gets or sets the directory in which to look for assemblies.
-        /// </summary>
-        public DirectoryInfo SearchDir { get; set; }
+//        /// <summary>
+//        /// Gets or sets the directory in which to look for assemblies.
+//        /// </summary>
+//        public DirectoryInfo SearchDir { get; set; }
 
-        /// <summary>
-        /// Gets or sets the recursive search depth.
-        /// </summary>
-        public Int32 SearchDepth { get; set; }
+//        /// <summary>
+//        /// Gets or sets the recursive search depth.
+//        /// </summary>
+//        public Int32 SearchDepth { get; set; }
 
-        /// <summary>
-        /// Gets or sets the search pattern for valid test assemblies.
-        /// </summary>
-        public String SearchPattern { get; set; }
+//        /// <summary>
+//        /// Gets or sets the search pattern for valid test assemblies.
+//        /// </summary>
+//        public String SearchPattern { get; set; }
 
-        /// <summary>
-        /// Gets or sets a list of directory names that are ignored.
-        /// </summary>
-        public List<String> IgnoredDirNames { get; } = new List<String>();
+//        /// <summary>
+//        /// Gets or sets a list of directory names that are ignored.
+//        /// </summary>
+//        public List<String> IgnoredDirNames { get; } = new List<String>();
 
-        #endregion
+//        #endregion
 
-    }
-}
+//    }
+//}
--- src/Nuclear.Test/Configurations/OutputConfiguration.cs
+++ src/Nuclear.Test/Configurations/OutputConfiguration.cs
@@ -1,41 +1,41 @@
-﻿using System;
+﻿//using System;
 
-using Nuclear.Test.Printer;
+//using Nuclear.Test.Printer;
 
-namespace Nuclear.Test.Configurations {
+//namespace Nuclear.Test.Configurations {
 
-    /// <summary>
-    /// Holds configuration values for output.
-    /// </summary>
-    public class OutputConfiguration {
+//    /// <summary>
+//    /// Holds configuration values for output.
+//    /// </summary>
+//    public class OutputConfiguration {
 
-        #region constants
+//        #region constants
 
-        /// <summary>
-        /// Configuration values for Verbosity.
-        /// </summary>
-        public const String VERBOSITY = "Output.Verbosity";
+//        /// <summary>
+//        /// Configuration values for Verbosity.
+//        /// </summary>
+//        public const String VERBOSITY = "Output.Verbosity";
 
-        /// <summary>
-        /// Configuration values for DiagnosticOutput.
-        /// </summary>
-        public const String DIAGNOSTIC_OUTPUT = "Output.DiagnosticOutput";
+//        /// <summary>
+//        /// Configuration values for DiagnosticOutput.
+//        /// </summary>
+//        public const String DIAGNOSTIC_OUTPUT = "Output.DiagnosticOutput";
 
-        #endregion
+//        #endregion
 
-        #region properties
+//        #region properties
 
-        /// <summary>
-        /// Gets or sets the verbosity of the results output.
-        /// </summary>
-        public Verbosity Verbosity { get; set; }
+//        /// <summary>
+//        /// Gets or sets the verbosity of the results output.
+//        /// </summary>
+//        public Verbosity Verbosity { get; set; }
 
-        /// <summary>
-        /// Gets or sets if diagnostic output should be visible in servers and clients.
-        /// </summary>
-        public Boolean DiagnosticOutput { get; set; }
+//        /// <summary>
+//        /// Gets or sets if diagnostic output should be visible in servers and clients.
+//        /// </summary>
+//        public Boolean DiagnosticOutput { get; set; }
 
-        #endregion
+//        #endregion
 
-    }
-}
+//    }
+//}
