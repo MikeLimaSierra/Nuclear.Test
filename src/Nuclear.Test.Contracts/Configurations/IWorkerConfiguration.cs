790ebcb45241990f5928a0345501bea7dbf99a5e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-24 10:51:10 +0200

    add test assembly path to worker config

--- src/Nuclear.Test.Contracts/Configurations/IWorkerConfiguration.cs
+++ src/Nuclear.Test.Contracts/Configurations/IWorkerConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using Nuclear.Test.Execution;
 
@@ -10,6 +11,11 @@ namespace Nuclear.Test.Configurations {
     public interface IWorkerConfiguration {
 
         #region properties
+
+        /// <summary>
+        /// Gets or sets the file path of the test assembly.
+        /// </summary>
+        FileInfo File { get; set; }
 
         /// <summary>
         /// Gets or sets if tests in an assembly should all be executed in a sequence.
--- src/Nuclear.Test/Configurations/WorkerConfiguration.cs
+++ src/Nuclear.Test/Configurations/WorkerConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using Nuclear.Test.Execution;
 
@@ -12,6 +13,11 @@ namespace Nuclear.Test.Configurations {
         #region constants
 
         /// <summary>
+        /// Configuration values for <see cref="File"/>.
+        /// </summary>
+        public const String FILE = "Worker.File";
+
+        /// <summary>
         /// Configuration values for <see cref="TestsInSequence"/>.
         /// </summary>
         public const String TESTS_IN_SEQUENCE = "Worker.TestsInSequence";
@@ -19,6 +25,11 @@ namespace Nuclear.Test.Configurations {
         #endregion
 
         #region properties
+
+        /// <summary>
+        /// Gets or sets the file path of the test assembly.
+        /// </summary>
+        public FileInfo File { get; set; }
 
         /// <summary>
         /// Gets or sets if tests in an assembly should all be executed in a sequence.
