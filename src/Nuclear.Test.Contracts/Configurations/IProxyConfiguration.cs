d65f715eaa8fe199314041ace9de6e7792040169
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-24 10:56:56 +0200

    create proxy configuration

---
+++ src/Nuclear.Test.Contracts/Configurations/IProxyConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+using Nuclear.Test.Execution;
+
+namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Defines configuration values for a proxy <see cref="IClient"/>.
+    /// </summary>
+    public interface IProxyConfiguration {
+
+        #region properties
+
+        /// <summary>
+        /// Gets or sets the file path of the test assembly.
+        /// </summary>
+        FileInfo File { get; set; }
+
+        /// <summary>
+        /// Gets or sets if assemblies should all be executed in a sequence.
+        ///     If set to true, there will only be one worker running at a time.
+        ///     If set to false, all proxies and workers will start immediately.
+        /// </summary>
+        Boolean AssembliesInSequence { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Test/Configurations/ProxyConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+using Nuclear.Test.Execution;
+
+namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Implements configuration values for a proxy <see cref="IClient"/>.
+    /// </summary>
+    public class ProxyConfiguration : IProxyConfiguration {
+
+        #region constants
+
+        /// <summary>
+        /// Configuration values for <see cref="File"/>.
+        /// </summary>
+        public const String FILE = "Worker.File";
+
+        /// <summary>
+        /// Configuration values for <see cref="AssembliesInSequence"/>.
+        /// </summary>
+        public const String ASSEMBLIES_IN_SEQUENCE = "Worker.AssembliesInSequence";
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Gets or sets the file path of the test assembly.
+        /// </summary>
+        public FileInfo File { get; set; }
+
+        /// <summary>
+        /// Gets or sets if assemblies should all be executed in a sequence.
+        ///     If set to true, there will only be one worker running at a time.
+        ///     If set to false, all proxies and workers will start immediately.
+        /// </summary>
+        public Boolean AssembliesInSequence { get; set; }
+
+        #endregion
+
+    }
+}
