6ea9e5ca13a8ff533b27cf0cb9d7bbc7a622fd92
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-28 22:36:21 +0200

    Update Program.cs

--- src/Nuclear.Test.Worker/Program.cs
+++ src/Nuclear.Test.Worker/Program.cs
@@ -1,27 +1,43 @@
 ﻿using System;
+using System.Threading;
 
-using Nuclear.Test.Output;
-using Nuclear.Test.Printer;
-using Nuclear.Test.Results;
-using Nuclear.Test.TestExecution;
+using Nuclear.Test.Link;
 
 namespace Nuclear.Test.Worker {
     static class Program {
 
+        #region fields
+
+        private static readonly ManualResetEventSlim _executionFinishedEvent = new ManualResetEventSlim(false);
+
+        private static WorkerClient _client;
+
+        #endregion
+
         #region public methods
 
         static void Main(String[] args) {
-            TestExecutor process = new TestWorker(args[0]);
-            ITestResultSource results = process.Execute();
+            _client = new WorkerClient(new ClientLink(args[0]));
+            _client.ExecutionFinished += OnClientExecutionFinished;
 
-            DiagnosticOutput.Log(process.OutputConfiguration, " =========================");
-            new ResultTree(process.OutputConfiguration.Verbosity, TestResultKey.Empty, results).Print();
-            DiagnosticOutput.Log(process.OutputConfiguration, "=========================");
+            _executionFinishedEvent.Wait();
 
-            if(process.OutputConfiguration.ClientsAwaitInput && process.OutputConfiguration.ShowClients) {
+            //TestExecutor process = new TestWorker(args[0]);
+            //ITestResultSource results = process.Execute();
+
+            //DiagnosticOutput.Log(process.OutputConfiguration, " =========================");
+            //new ResultTree(process.OutputConfiguration.Verbosity, TestResultKey.Empty, results).Print();
+            //DiagnosticOutput.Log(process.OutputConfiguration, "=========================");
+
+            if(!_client.Configuration.AutoShutdown) {
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey(true);
             }
+        }
+
+        private static void OnClientExecutionFinished(Object sender, EventArgs e) {
+            _client.ExecutionFinished -= OnClientExecutionFinished;
+            _executionFinishedEvent.Set();
         }
 
         #endregion
