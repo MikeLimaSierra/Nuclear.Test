7175a8f88376f9634c7c0884936cc0c788f9ac25
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-09-28 22:51:37 +0200

    Create WorkerRemote.cs

---
+++ src/Nuclear.Test.Proxy/WorkerRemote.cs
@@ -0,0 +1,33 @@
+﻿
+using Nuclear.Test.Configurations;
+using Nuclear.Test.Execution;
+using Nuclear.Test.Link;
+
+namespace Nuclear.Test.Proxy {
+    class WorkerRemote : Remote {
+
+        #region ctors
+
+        public WorkerRemote(IRemoteConfiguration remoteConfig, IClientConfiguration clientConfig, IServerLink link)
+            : base(remoteConfig, clientConfig, link) {
+
+        }
+
+        #endregion
+
+        #region methods
+
+        public override void Execute() {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IMessage GetSetupMessage(IMessage message) {
+            message = base.GetSetupMessage(message);
+
+            return message;
+        }
+
+        #endregion
+
+    }
+}
