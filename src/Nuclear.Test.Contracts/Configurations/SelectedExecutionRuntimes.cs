5c1a753d63cc99e906d150bb1b54074f4d2054f1
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-10-07 15:23:27 +0200

    Create SelectedExecutionRuntimes.cs

---
+++ src/Nuclear.Test.Contracts/Configurations/SelectedExecutionRuntimes.cs
@@ -0,0 +1,23 @@
+﻿namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Defines a range of targeted execution runtimes for test assemblies.
+    /// </summary>
+    public enum SelectedExecutionRuntimes {
+
+        /// <summary>
+        /// Run tests on lowest possible runtime versions.
+        /// </summary>
+        Lowest,
+
+        /// <summary>
+        /// Run tests on highest possible runtime versions.
+        /// </summary>
+        Highest,
+
+        /// <summary>
+        /// Run tests on all possible runtime versions.
+        /// </summary>
+        All,
+    }
+}
