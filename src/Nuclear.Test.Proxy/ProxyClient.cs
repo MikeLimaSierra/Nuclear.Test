608eaacaf332a5ccddd41573f02a013cf11ed4b2
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-10-07 11:23:12 +0200

    Create ProxyClient.cs

---
+++ src/Nuclear.Test.Proxy/ProxyClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Nuclear.Test.Configurations;
+using Nuclear.Test.Execution;
+using Nuclear.Test.Link;
+
+namespace Nuclear.Test.Proxy {
+    internal class ProxyClient : Client {
+
+        #region fields
+
+        private IProxyConfiguration _proxyConfig;
+
+        #endregion
+
+        #region ctors
+
+        public ProxyClient(IClientLink link)
+            : base(link) {
+
+            HeaderContent.Add(@" _   _               _                    _____           _   ");
+            HeaderContent.Add(@"| \ | | _   _   ___ | |  ___   __ _  _ __|_   _|___  ___ | |_ ");
+            HeaderContent.Add(@"|  \| || | | | / __|| | / _ \ / _` || '__| | | / _ \/ __|| __|");
+            HeaderContent.Add(@"| |\  || |_| || (__ | ||  __/| (_| || | _  | ||  __/\__ \| |_ ");
+            HeaderContent.Add(@"|_| \_| \__,_| \___||_| \___| \__,_||_|(_) |_| \___||___/ \__|");
+            HeaderContent.Add(@" ____                                                         ");
+            HeaderContent.Add(@"|  _ \  _ __  ___ __  __ _   _                                ");
+            HeaderContent.Add(@"| |_) || '__|/ _ \\ \/ /| | | |                               ");
+            HeaderContent.Add(@"|  __/ | |  | (_) |>  < | |_| |                               ");
+            HeaderContent.Add(@"|_|    |_|   \___//_/\_\ \__, |                               ");
+            HeaderContent.Add(@"                         |___/                                ");
+            HeaderContent.Add(@"                                                              ");
+        }
+
+        #endregion
+
+        #region methods
+
+        protected override void Setup(IMessage message) {
+            base.Setup(message);
+
+            message.TryGetData(out _proxyConfig);
+        }
+
+        protected override void Execute() {
+            base.Execute();
+
+            // todo
+
+            SendFinished();
+            Link.WaitForOutputFlush();
+            RaiseExecutionFinished();
+        }
+
+        #endregion
+
+    }
+}
