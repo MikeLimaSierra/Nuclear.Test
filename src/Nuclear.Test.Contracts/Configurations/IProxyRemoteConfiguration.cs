9e5097b73e5d28e66dda10302685e4a5b90ac32a
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-10-09 11:54:08 +0200

    add remote conig definitions for worker & proxy

---
+++ src/Nuclear.Test.Contracts/Configurations/IProxyRemoteConfiguration.cs
@@ -0,0 +1,16 @@
+﻿
+using Nuclear.Test.Execution;
+
+namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Defines configuration values for an <see cref="IRemote{TConfiguration}"/>.
+    /// </summary>
+    public interface IProxyRemoteConfiguration : IRemoteConfiguration {
+
+        #region properties
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Test.Contracts/Configurations/IWorkerRemoteConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Nuclear.Test.Execution;
+
+namespace Nuclear.Test.Configurations {
+
+    /// <summary>
+    /// Defines configuration values for an <see cref="IRemote{TConfiguration}"/>.
+    /// </summary>
+    public interface IWorkerRemoteConfiguration : IRemoteConfiguration {
+
+        #region properties
+
+        #endregion
+
+    }
+}
