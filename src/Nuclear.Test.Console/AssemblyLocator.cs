a1b166a4c6b0d4c16edb42c842ede7c9f40116ff
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-11-25 11:42:32 +0100

    move assembly locator

---
+++ src/Nuclear.Test.Console/AssemblyLocator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using log4net;
+
+using Nuclear.Extensions;
+
+namespace Nuclear.Test.Console {
+    internal class AssemblyLocator {
+
+        #region fields
+
+        private static readonly ILog _log = LogManager.GetLogger(typeof(AssemblyLocator));
+
+        private DirectoryInfo _searchDirectory = null;
+
+        #endregion
+
+        #region properties
+
+        internal DirectoryInfo SearchDirectory {
+            get => _searchDirectory ??= new FileInfo(Assembly.GetEntryAssembly().Location).Directory;
+            set => _searchDirectory = value;
+        }
+
+        internal Int32 SearchDepth { get; set; }
+
+        internal String SearchPattern { get; set; } = "*Tests.dll";
+
+        internal IList<String> IgnoredDirectoryNames { get; } = new List<String>();
+
+        #endregion
+
+        #region methods
+
+        internal IEnumerable<FileInfo> DiscoverAssemblies()
+            => SearchDirectory != null && SearchDirectory.Exists ? DiscoverAssemblies(SearchDirectory, SearchDepth) : Enumerable.Empty<FileInfo>();
+
+        private IEnumerable<FileInfo> DiscoverAssemblies(DirectoryInfo directory, Int32 depth) {
+            List<FileInfo> files = directory.EnumerateFiles(SearchPattern).ToList();
+
+            if(depth != 0) {
+                directory
+                    .EnumerateDirectories()
+                    .Where(d => !IgnoredDirectoryNames.Contains(d.Name))
+                    .Foreach(d => files.AddRange(DiscoverAssemblies(d, depth - 1)));
+            }
+
+            return files;
+        }
+
+        #endregion
+
+    }
+}
