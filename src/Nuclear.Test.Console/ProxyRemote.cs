5af9adcf0b293385ca1cf06fbeb9f490415d6d62
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-11-26 13:51:39 +0100

    Create ProxyRemote.cs

---
+++ src/Nuclear.Test.Console/ProxyRemote.cs
@@ -0,0 +1,34 @@
+﻿using log4net;
+
+using Nuclear.Test.Configurations;
+using Nuclear.Test.Execution;
+using Nuclear.Test.Link;
+
+namespace Nuclear.Test.Console {
+    internal class ProxyRemote : Remote<IProxyRemoteConfiguration, IProxyClientConfiguration> {
+
+        #region fields
+
+        private static readonly ILog _log = LogManager.GetLogger(typeof(ProxyRemote));
+
+        #endregion
+
+        #region ctors
+
+        public ProxyRemote(IProxyRemoteConfiguration remoteConfig, IProxyClientConfiguration clientConfiguration, IServerLink link)
+            : base(remoteConfig, clientConfiguration, link) { }
+
+        #endregion
+
+        #region methods
+
+        protected override IMessage GetSetupMessage() {
+            _log.Debug(nameof(GetSetupMessage));
+
+            return base.GetSetupMessage().Append(ClientConfiguration);
+        }
+
+        #endregion
+
+    }
+}
