6f2dd20bc6a7c8aa9a9dca82cbfaf4274cf99499
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-11-26 12:46:11 +0100

    Create Executer.cs

---
+++ src/Nuclear.Test.Console/Executer.cs
@@ -0,0 +1,45 @@
+﻿using Nuclear.Exceptions;
+using Nuclear.Test.Results;
+
+namespace Nuclear.Test.Console {
+    internal class Executer {
+
+        #region fields
+
+        private readonly Configuration _configuration;
+
+        private readonly IFactory _factory;
+
+        #endregion
+
+        #region properties
+
+        internal ITestResultEndPoint Results { get; }
+
+        #endregion
+
+        #region ctors
+
+        public Executer(Configuration configuration, IFactory factory) {
+            Throw.If.Object.IsNull(configuration, nameof(configuration));
+            Throw.If.Object.IsNull(factory, nameof(factory));
+
+            _configuration = configuration;
+            _factory = factory;
+
+            _factory.Create(out ITestResultEndPoint results);
+            Results = results;
+        }
+
+        #endregion
+
+        #region methods
+
+        internal void Execute() {
+            // todo
+        }
+
+        #endregion
+
+    }
+}
