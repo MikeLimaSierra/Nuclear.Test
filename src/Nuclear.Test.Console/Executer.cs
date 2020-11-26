d7753ca669068cac0994e52c808785706ef0adf3
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-11-26 13:20:57 +0100

    Update Executer.cs

--- src/Nuclear.Test.Console/Executer.cs
+++ src/Nuclear.Test.Console/Executer.cs
@@ -1,4 +1,14 @@
-﻿using Nuclear.Exceptions;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+using log4net;
+
+using Nuclear.Assemblies;
+using Nuclear.Assemblies.Runtimes;
+using Nuclear.Exceptions;
+using Nuclear.Test.Helpers;
 using Nuclear.Test.Results;
 
 namespace Nuclear.Test.Console {
@@ -6,9 +16,17 @@ namespace Nuclear.Test.Console {
 
         #region fields
 
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Executer));
+
         private readonly Configuration _configuration;
 
         private readonly IFactory _factory;
+
+        private readonly List<String> _headerContent = new List<String>();
+
+        private readonly AssemblyLocator _locator;
+
+        private readonly CountdownEvent _remotesFinishedEvent = new CountdownEvent(0);
 
         #endregion
 
@@ -29,6 +47,25 @@ namespace Nuclear.Test.Console {
 
             _factory.Create(out ITestResultEndPoint results);
             Results = results;
+
+            _headerContent.Add(@" _   _               _                    _____           _   ");
+            _headerContent.Add(@"| \ | | _   _   ___ | |  ___   __ _  _ __|_   _|___  ___ | |_ ");
+            _headerContent.Add(@"|  \| || | | | / __|| | / _ \ / _` || '__| | | / _ \/ __|| __|");
+            _headerContent.Add(@"| |\  || |_| || (__ | ||  __/| (_| || | _  | ||  __/\__ \| |_ ");
+            _headerContent.Add(@"|_| \_| \__,_| \___||_| \___| \__,_||_|(_) |_| \___||___/ \__|");
+            _headerContent.Add(@"  ____                           _                            ");
+            _headerContent.Add(@" / ___| ___   _ __   ___   ___  | |  ___                      ");
+            _headerContent.Add(@"| |    / _ \ | '_ \ / __| / _ \ | | / _ \                     ");
+            _headerContent.Add(@"| |___| (_) || | | |\__ \| (_) || ||  __/                     ");
+            _headerContent.Add(@" \____|\___/ |_| |_||___/ \___/ |_| \___|                     ");
+            _headerContent.Add(@"                                                              ");
+
+            _locator = new AssemblyLocator() {
+                SearchDirectory = new DirectoryInfo(Environment.ExpandEnvironmentVariables(_configuration.Locator.SearchDirectory)),
+                SearchDepth = _configuration.Locator.SearchDepth,
+                SearchPattern = _configuration.Locator.SearchPattern,
+                IgnoredDirectoryNames = _configuration.Locator.IgnoredDirectoryNames
+            };
         }
 
         #endregion
@@ -36,7 +73,34 @@ namespace Nuclear.Test.Console {
         #region methods
 
         internal void Execute() {
-            // todo
+            RuntimesHelper.TryGetCurrentRuntime(out RuntimeInfo currentRuntime);
+
+            ConsoleHelper.SetConsoleTitle(currentRuntime);
+            ConsoleHelper.PrintProcessInfo(currentRuntime, _headerContent);
+
+            IEnumerable<FileInfo> assemblies = _locator.DiscoverAssemblies();
+            IEnumerable<RemoteInfo> remoteInfos = CreateRemoteInfos(assemblies);
+            IEnumerable<ProxyRemote> remotes = CreateRemotes(remoteInfos);
+
+            foreach(ProxyRemote remote in remotes) {
+                _remotesFinishedEvent.AddCount();
+
+                remote.Execute();
+
+                if(_configuration.Execution.AssembliesInSequence) {
+                    _remotesFinishedEvent.Wait();
+                }
+            }
+
+            _remotesFinishedEvent.Wait();
+        }
+
+        private IEnumerable<RemoteInfo> CreateRemoteInfos(IEnumerable<FileInfo> assemblies) {
+            return null;
+        }
+
+        private IEnumerable<ProxyRemote> CreateRemotes(IEnumerable<RemoteInfo> remoteInfos) {
+            return null;
         }
 
         #endregion
