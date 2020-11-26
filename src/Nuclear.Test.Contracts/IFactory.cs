172f967e103ed6254c9875eb5aa67e084775531b
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-11-26 12:45:54 +0100

    redo factory

---
+++ src/Nuclear.Test.Contracts/IFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+using Nuclear.Assemblies.Runtimes;
+using Nuclear.Test.Results;
+
+namespace Nuclear.Test {
+    public interface IFactory {
+
+        #region results
+
+        void Create(out ITestEntry @object, EntryTypes type, String instruction, String message);
+
+        void Create(out ITestMethodResult @object);
+
+        void Create(out ITestResultEndPoint @object);
+
+        void CreateEmpty(out ITestResultKey @object);
+
+        void Create(out ITestResultKey @object, ITestScenario scenario, MethodInfo methodInfo);
+
+        void Create(out ITestResultKey @object, ITestScenario scenario, String fileName, String methodName);
+
+        void Create(out ITestResultKey @object,
+            String assemblyName,
+            RuntimeInfo targetRuntime,
+            ProcessorArchitecture targetArchitecture,
+            RuntimeInfo executionRuntime,
+            ProcessorArchitecture executionArchitecture,
+            String fileName,
+            String methodName);
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Test/Factory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+using Nuclear.Assemblies.Runtimes;
+using Nuclear.Test.Results;
+
+namespace Nuclear.Test {
+
+    /// <summary>
+    /// A factory for result related implementations.
+    /// </summary>
+    public class Factory : IFactory {
+
+        #region properties
+
+        public static IFactory Instance { get; } = new Factory();
+
+        #endregion
+
+        #region ctors
+
+        private Factory() { }
+
+        #endregion
+
+        #region results
+
+        public void Create(out ITestEntry @object, EntryTypes type, String instruction, String message) => @object = new TestEntry(type, instruction, message);
+
+        public void Create(out ITestMethodResult @object) => @object = new TestMethodResult();
+
+        public void Create(out ITestResultEndPoint @object) => @object = new TestResultEndPoint();
+
+        public void CreateEmpty(out ITestResultKey @object) => @object = TestResultKey.Empty;
+
+        public void Create(out ITestResultKey @object, ITestScenario scenario, MethodInfo methodInfo) => @object = new TestResultKey(scenario, methodInfo);
+
+        public void Create(out ITestResultKey @object, ITestScenario scenario, String fileName, String methodName) => @object = new TestResultKey(scenario, fileName, methodName);
+
+        public void Create(out ITestResultKey @object,
+            String assemblyName,
+            RuntimeInfo targetRuntime,
+            ProcessorArchitecture targetArchitecture,
+            RuntimeInfo executionRuntime,
+            ProcessorArchitecture executionArchitecture,
+            String fileName,
+            String methodName) => @object = new TestResultKey(assemblyName, targetRuntime, targetArchitecture, executionRuntime, executionArchitecture, fileName, methodName);
+
+        #endregion
+
+    }
+}
