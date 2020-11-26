3ec6826864c582a077abe0afe84edb76053aa41a
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-11-26 12:24:26 +0100

    Create Factory.cs

---
+++ src/Nuclear.Test/Results/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+using Nuclear.Assemblies.Runtimes;
+
+namespace Nuclear.Test.Results {
+
+    /// <summary>
+    /// A factory for result related implementations.
+    /// </summary>
+    public static class Factory {
+
+        #region methods
+
+        public static void Create(out ITestEntry @object, EntryTypes type, String instruction, String message) => @object = new TestEntry(type, instruction, message);
+
+        public static void Create(out ITestMethodResult @object) => @object = new TestMethodResult();
+
+        public static void Create(out ITestResultEndPoint @object) => @object = new TestResultEndPoint();
+
+        public static void Create(out ITestResultKey @object, ITestScenario scenario, MethodInfo methodInfo) => @object = new TestResultKey(scenario, methodInfo);
+
+        public static void Create(out ITestResultKey @object, ITestScenario scenario, String fileName, String methodName) => @object = new TestResultKey(scenario, fileName, methodName);
+
+        public static void Create(out ITestResultKey @object,
+            String assemblyName,
+            RuntimeInfo targetRuntime,
+            ProcessorArchitecture targetArchitecture,
+            RuntimeInfo executionRuntime,
+            ProcessorArchitecture executionArchitecture,
+            String fileName,
+            String methodName) => @object = new TestResultKey(assemblyName, targetRuntime, targetArchitecture, executionRuntime, executionArchitecture, fileName, methodName);
+
+        #endregion
+
+    }
+}
