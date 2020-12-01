07e35fdcb9c9fde4ac89da36c3e891a64247591c
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-12-01 12:54:14 +0100

    update publish scripts to %APPDATA%

--- publish_integration_console.bat
+++ publish_integration_console.bat
@@ -1,3 +1,3 @@
-dotnet publish --self-contained true -r win-x86 -c Integration -f netcoreapp2.0 -o publish\Nuclear.Test.Console\ src\Nuclear.Test.Console\Nuclear.Test.Console.csproj
+dotnet publish --self-contained true -r win-x86 -c Integration -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Console\ src\Nuclear.Test.Console\Nuclear.Test.Console.csproj
 
 pause
--- publish_release.bat
+++ publish_release.bat
@@ -1,38 +1,46 @@
-rmdir /S /Q publish
+rmdir /S /Q %APPDATA%\Nuclear.Test.Worker
+rmdir /S /Q %APPDATA%\Nuclear.Test.Proxy
+rmdir /S /Q %APPDATA%\Nuclear.Test.Console
 
-dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp2.0 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETCoreApp2.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.0 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETCoreApp2.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp2.0 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NETCoreApp2.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.0 -o %APPDATA%\Nuclear.Test.Worker\X86\NETCoreApp2.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp2.1 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETCoreApp2.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.1 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETCoreApp2.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp2.1 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NETCoreApp2.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.1 -o %APPDATA%\Nuclear.Test.Worker\X86\NETCoreApp2.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp2.2 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETCoreApp2.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.2 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETCoreApp2.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp2.2 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NETCoreApp2.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.2 -o %APPDATA%\Nuclear.Test.Worker\X86\NETCoreApp2.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp3.0 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETCoreApp3.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp3.0 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETCoreApp3.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp3.0 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NETCoreApp3.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp3.0 -o %APPDATA%\Nuclear.Test.Worker\X86\NETCoreApp3.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-dotnet publish -r win-x64 -c Release -f net461 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.6.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish -r win-x86 -c Release -f net461 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.6.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NETCoreApp3.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Worker\X86\NETCoreApp3.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-dotnet publish -r win-x64 -c Release -f net462 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.6.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish -r win-x86 -c Release -f net462 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.6.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x64 -c Release -f net5.0 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NET5.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f net5.0 -o %APPDATA%\Nuclear.Test.Worker\X86\NET5.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-dotnet publish -r win-x64 -c Release -f net47 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish -r win-x86 -c Release -f net47 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+xcopy /Y bin\Nuclear.Test.Worker\x64\Release\net461\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.6.1\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Release\net461\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.6.1\
 
-dotnet publish -r win-x64 -c Release -f net471 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish -r win-x86 -c Release -f net471 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7.1\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+xcopy /Y bin\Nuclear.Test.Worker\x64\Release\net462\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.6.2\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Release\net462\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.6.2\
 
-dotnet publish -r win-x64 -c Release -f net472 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish -r win-x86 -c Release -f net472 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7.2\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+xcopy /Y bin\Nuclear.Test.Worker\x64\Release\net47\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Release\net47\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7\
 
-dotnet publish -r win-x64 -c Release -f net48 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.8\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
-dotnet publish -r win-x86 -c Release -f net48 -o publish\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.8\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
+xcopy /Y bin\Nuclear.Test.Worker\x64\Release\net471\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7.1\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Release\net471\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7.1\
 
-dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp3.0 -o publish\Nuclear.Test.Console\Nuclear.Test.Proxy\Amd64\ src\Nuclear.Test.Proxy\Nuclear.Test.Proxy.csproj
-dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp3.0 -o publish\Nuclear.Test.Console\Nuclear.Test.Proxy\X86\ src\Nuclear.Test.Proxy\Nuclear.Test.Proxy.csproj
+xcopy /Y bin\Nuclear.Test.Worker\x64\Release\net472\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7.2\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Release\net472\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7.2\
 
-dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp2.0 -o publish\Nuclear.Test.Console\ src\Nuclear.Test.Console\Nuclear.Test.Console.csproj
+xcopy /Y bin\Nuclear.Test.Worker\x64\Release\net48\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.8\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Release\net48\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.8\
+
+dotnet publish --self-contained true -r win-x64 -c Release -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Proxy\Amd64\ src\Nuclear.Test.Proxy\Nuclear.Test.Proxy.csproj
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Proxy\X86\ src\Nuclear.Test.Proxy\Nuclear.Test.Proxy.csproj
+
+dotnet publish --self-contained true -r win-x86 -c Release -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Console\ src\Nuclear.Test.Console\Nuclear.Test.Console.csproj
 
 pause
