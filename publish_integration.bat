02b77addcc29f1a519694bf59ab90a09b9e58e84
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-12-01 14:09:57 +0100

    fix publish paths

--- publish_integration.bat
+++ publish_integration.bat
@@ -20,23 +20,23 @@ dotnet publish --self-contained true -r win-x86 -c Integration -f netcoreapp3.1 
 dotnet publish --self-contained true -r win-x64 -c Integration -f net5.0 -o %APPDATA%\Nuclear.Test.Worker\Amd64\NET5.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 dotnet publish --self-contained true -r win-x86 -c Integration -f net5.0 -o %APPDATA%\Nuclear.Test.Worker\X86\NET5.0\ src\Nuclear.Test.Worker\Nuclear.Test.Worker.csproj
 
-xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net461\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.6.1\
-xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net461\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.6.1\
+xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net461\* %APPDATA%\Nuclear.Test.Worker\Amd64\NETFramework4.6.1\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net461\* %APPDATA%\Nuclear.Test.Worker\X86\NETFramework4.6.1\
 
-xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net462\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.6.2\
-xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net462\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.6.2\
+xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net462\* %APPDATA%\Nuclear.Test.Worker\Amd64\NETFramework4.6.2\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net462\* %APPDATA%\Nuclear.Test.Worker\X86\NETFramework4.6.2\
 
-xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net47\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7\
-xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net47\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7\
+xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net47\* %APPDATA%\Nuclear.Test.Worker\Amd64\NETFramework4.7\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net47\* %APPDATA%\Nuclear.Test.Worker\X86\NETFramework4.7\
 
-xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net471\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7.1\
-xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net471\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7.1\
+xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net471\* %APPDATA%\Nuclear.Test.Worker\Amd64\NETFramework4.7.1\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net471\* %APPDATA%\Nuclear.Test.Worker\X86\NETFramework4.7.1\
 
-xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net472\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.7.2\
-xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net472\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.7.2\
+xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net472\* %APPDATA%\Nuclear.Test.Worker\Amd64\NETFramework4.7.2\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net472\* %APPDATA%\Nuclear.Test.Worker\X86\NETFramework4.7.2\
 
-xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net48\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\Amd64\NETFramework4.8\
-xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net48\* %APPDATA%\Nuclear.Test.Console\Nuclear.Test.Worker\X86\NETFramework4.8\
+xcopy /Y bin\Nuclear.Test.Worker\x64\Integration\net48\* %APPDATA%\Nuclear.Test.Worker\Amd64\NETFramework4.8\
+xcopy /Y bin\Nuclear.Test.Worker\x86\Integration\net48\* %APPDATA%\Nuclear.Test.Worker\X86\NETFramework4.8\
 
 dotnet publish --self-contained true -r win-x64 -c Integration -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Proxy\Amd64\ src\Nuclear.Test.Proxy\Nuclear.Test.Proxy.csproj
 dotnet publish --self-contained true -r win-x86 -c Integration -f netcoreapp3.1 -o %APPDATA%\Nuclear.Test.Proxy\X86\ src\Nuclear.Test.Proxy\Nuclear.Test.Proxy.csproj
