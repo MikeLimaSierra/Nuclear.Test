64b63d7dc8beec4938385fb7cd771b761f051bb4
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-12-17 23:24:12 +0100

    Update ArchitectureFilterList.cs

--- src/Nuclear.Test.Console/Filters/ArchitectureFilterList.cs
+++ src/Nuclear.Test.Console/Filters/ArchitectureFilterList.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Nuclear.Test.Console.Filters {
     internal class ArchitectureFilterList : FilterList {
 
         #region properties
 
-        [JsonProperty]
-        //[JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty(ItemConverterType = typeof(StringEnumConverter))]
         internal IList<ProcessorArchitecture> Values { get; set; }
 
         #endregion
