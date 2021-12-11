93d5a68e9ef40ab5084423bcf0b2fd763b42106e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-12-11 22:37:52 +0100

    simplify worker configuration

---
+++ src/Nuclear.Test.Worker/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using Nuclear.Test.Configurations;
+
+namespace Nuclear.Test.Worker {
+
+    /// <summary>
+    /// Holds configuration values for an <see cref="IWorkerClient"/>.
+    /// </summary>
+    public sealed class Configuration : ClientConfiguration {
+
+        #region properties
+
+        /// <summary>
+        /// Gets or sets how tests in an assembly should be executed.
+        /// </summary>
+        public TestModeOverrides TestMethodModeOverride { get; set; } = TestModeOverrides.Auto;
+
+        #endregion
+
+    }
+}
