73037fd7a80e5dcfda74a7f9479fddcb24be72d6
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-12-11 22:42:54 +0100

    simplify proxy configuration

--- src/Nuclear.Test.Proxy/Configuration.cs
+++ src/Nuclear.Test.Proxy/Configuration.cs
@@ -3,47 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 
 using Nuclear.Assemblies.Runtimes;
-using Nuclear.Test.Configurations.Worker;
-using Nuclear.Test.Execution.Proxy;
+using Nuclear.Test.Configurations;
 
-namespace Nuclear.Test.Configurations.Proxy {
+namespace Nuclear.Test.Proxy {
 
     /// <summary>
-    /// Defines configuration values for an <see cref="IProxyClient"/>.
+    /// Holds configuration values for an <see cref="IProxyClient"/>.
     /// </summary>
-    public interface IProxyClientConfiguration : IClientConfiguration {
+    public sealed class Configuration : ClientConfiguration {
 
         #region properties
 
         /// <summary>
         /// Gets or sets how assemblies should be executed.
         /// </summary>
-        TestModeOverrides AssemblyModeOverride { get; set; }
+        public TestModeOverrides AssemblyModeOverride { get; set; } = TestModeOverrides.Auto;
 
         /// <summary>
         /// Gets or sets the runtimes that can be used.
         /// </summary>
-        IEnumerable<RuntimeInfo> AvailableRuntimes { get; set; }
+        public IEnumerable<RuntimeInfo> AvailableRuntimes { get; set; }
 
         /// <summary>
         /// Gets or sets the selected runtimes that are used to execute tests.
         /// </summary>
-        SelectedExecutionRuntimes SelectedRuntimes { get; set; }
+        public SelectedExecutionRuntimes SelectedRuntimes { get; set; } = SelectedExecutionRuntimes.Reasonable;
 
         /// <summary>
         /// Gets or sets the directory that contains all workers.
         /// </summary>
-        DirectoryInfo WorkerDirectory { get; set; }
+        public DirectoryInfo WorkerDirectory { get; set; }
 
         /// <summary>
         /// Gets or sets the name of any worker executable.
         /// </summary>
-        String WorkerExecutableName { get; set; }
-
-        /// <summary>
-        /// Gets or sets the worker remote configuration object.
-        /// </summary>
-        IWorkerRemoteConfiguration WorkerRemoteConfiguration { get; set; }
+        public String WorkerExecutableName { get; set; }
 
         #endregion
 
