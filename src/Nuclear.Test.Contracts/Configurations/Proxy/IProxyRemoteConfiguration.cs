01f33dadea6063acc6c6fac8caefa052106908f9
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-12-11 22:43:25 +0100

    get rid of useless remote configurations

--- src/Nuclear.Test.Contracts/Configurations/Proxy/IProxyRemoteConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nuclear.Test.Execution.Proxy;
-
-namespace Nuclear.Test.Configurations.Proxy {
-
-    /// <summary>
-    /// Defines configuration values for an <see cref="IProxyRemote"/>.
-    /// </summary>
-    public interface IProxyRemoteConfiguration : IRemoteConfiguration<IProxyClientConfiguration> { }
-}
--- src/Nuclear.Test.Contracts/Configurations/Worker/IWorkerRemoteConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nuclear.Test.Execution.Worker;
-
-namespace Nuclear.Test.Configurations.Worker {
-
-    /// <summary>
-    /// Defines configuration values for an <see cref="IWorkerRemote"/>.
-    /// </summary>
-    public interface IWorkerRemoteConfiguration : IRemoteConfiguration<IWorkerClientConfiguration> { }
-}
