4ba373981f763fc47f3807e62822a6b619e038af
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-12-11 23:01:45 +0100

    Update ClientConfiguration.cs

--- src/Nuclear.Test.Contracts/Configurations/ClientConfiguration.cs
+++ src/Nuclear.Test.Contracts/Configurations/ClientConfiguration.cs
@@ -22,12 +22,12 @@ namespace Nuclear.Test.Configurations {
         ///     If set to true, the client will automatically shut down.
         ///     If set to false, the client will will wait for console input.
         /// </summary>
-        public Boolean AutoShutdown { get; set; }
+        public Boolean AutoShutdown { get; set; } = true;
 
         /// <summary>
         /// Gets or sets if the client will write all results into a detailed report file.
         /// </summary>
-        public Boolean WriteReport { get; set; }
+        public Boolean WriteReport { get; set; } = false;
 
         #endregion
 
