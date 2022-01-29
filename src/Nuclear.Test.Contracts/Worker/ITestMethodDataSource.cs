c6ca1e68b8699387ab85c33d85ed0ff29568d7b3
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-01-29 22:35:57 +0100

    Create ITestMethodDataSource.cs

---
+++ src/Nuclear.Test.Contracts/Worker/ITestMethodDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nuclear.Test.Worker {
+
+    /// <summary>
+    /// Describes a test data source for test methods.
+    /// </summary>
+    public interface ITestMethodDataSource {
+
+        #region methods
+
+        /// <summary>
+        /// Gets the test data from the source object.
+        /// </summary>
+        /// <returns>A collection of test data sets.</returns>
+        IEnumerable<Object[]> GetData();
+
+        #endregion
+
+    }
+}
