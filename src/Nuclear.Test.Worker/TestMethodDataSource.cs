7c4d91eb1ed7241d7b506ef9d0509df7d523c00e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-03 18:26:55 +0100

    Create TestMethodDataSource.cs

---
+++ src/Nuclear.Test.Worker/TestMethodDataSource.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Nuclear.Exceptions;
+
+namespace Nuclear.Test.Worker {
+
+    /// <summary>
+    /// A delegate used to return test data sets.
+    /// </summary>
+    /// <returns>A collection of test data sets.</returns>
+    public delegate IEnumerable<Object[]> GetTestData();
+
+    internal class TestMethodDataSource : ITestMethodDataSource {
+
+        #region fields
+
+        private readonly GetTestData _delegate;
+
+        #endregion
+
+        #region ctors
+
+        internal TestMethodDataSource(GetTestData @delegate) {
+            Throw.If.Object.IsNull(@delegate, nameof(@delegate));
+
+            _delegate = @delegate;
+        }
+
+        #endregion
+
+        #region methods
+
+        public IEnumerable<ITestDataSet> GetData() {
+            IEnumerable<Object[]> dataSets = null;
+
+            try {
+                dataSets = _delegate();
+
+            } catch { /* TODO: add logging */ }
+
+            if(dataSets != null) {
+                foreach(Object[] data in dataSets) {
+                    yield return new TestDataSet(data);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
