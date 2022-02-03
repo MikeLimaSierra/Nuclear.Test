edf3cc38df2d8eeb4e53a6512622db7c8335f74f
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-03 18:25:30 +0100

    Create ITestDataSet.cs

---
+++ src/Nuclear.Test.Worker.Contracts/ITestDataSet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker {
+
+    /// <summary>
+    /// Defines one set of test data objects.
+    /// </summary>
+    public interface ITestDataSet {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets if test data objects are available in the set.
+        /// </summary>
+        Boolean HasData { get; }
+
+        /// <summary>
+        /// Gets the number of objects in the set.
+        /// </summary>
+        Int32 ObjectCount { get; }
+
+        /// <summary>
+        /// Gets the number of leading type objects.
+        /// </summary>
+        Int32 LeadingTypeCount { get; }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Gets the objects of the set.
+        /// </summary>
+        /// <param name="data">The test data objects.</param>
+        /// <returns>True if values can be returned.</returns>
+        Boolean GetObjects(out Object[] data);
+
+        /// <summary>
+        /// Gets the objects of the test set split into types and data.
+        /// </summary>
+        /// <param name="typeCount">The number of expected leading types.</param>
+        /// <param name="types">The type objects used for generics.</param>
+        /// <param name="data">The test data objects.</param>
+        /// <returns>True if values can be returned.</returns>
+        Boolean GetObjects(UInt32 typeCount, out Type[] types, out Object[] data);
+
+        #endregion
+
+    }
+}
