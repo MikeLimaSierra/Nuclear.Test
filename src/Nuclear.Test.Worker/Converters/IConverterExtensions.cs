0cf4cdf2647461632ea37bd2d1de4ee276232382
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-05 12:33:56 +0100

    Create IConverterExtensions.cs

---
+++ src/Nuclear.Test.Worker/Converters/IConverterExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using Nuclear.Creation;
+using Nuclear.Extensions;
+
+namespace Nuclear.Test.Worker.Converters {
+
+    /// <summary>
+    /// Defines an extension to <see cref="IConverter"/>.
+    /// </summary>
+    public static class IConverterExtensions {
+
+        /// <summary>
+        /// Returns a new instance of <see cref="ICreator{String, Attribute}"/>.
+        /// </summary>
+        /// <returns>A  new instance of <see cref="ICreator{String, Attribute}"/>.</returns>
+        public static ICreator<String, Attribute> Attributes(this IConverter _) => Factory.Instance.Creator.Create((Attribute in1) => Convert(in1));
+
+        internal static String Convert(Attribute attr) {
+            Type attrType = attr.GetType();
+            IOrderedEnumerable<PropertyInfo> attrProps = attrType.GetMembers(BindingFlags.Public | BindingFlags.Instance)
+                .Where(_ => _ is PropertyInfo)
+                .Where(_ => _.Name != nameof(Attribute.TypeId))
+                .Cast<PropertyInfo>()
+                .ToList()
+                .OrderBy(_ => _.Name);
+
+            String attrName = attrType.Name.EndsWith(nameof(Attribute)) ? attrType.Name.Substring(0, attrType.Name.LastIndexOf(nameof(Attribute))) : attrType.Name;
+            String propertiesString = attrProps.Any() ? $"({String.Join(", ", attrProps.Select(_ => $"{_.Name} = {_.GetMethod.Invoke(attr, new Object[0]).Format()}"))})" : String.Empty;
+
+            return $"[{attrName}{propertiesString}]";
+        }
+
+    }
+}
