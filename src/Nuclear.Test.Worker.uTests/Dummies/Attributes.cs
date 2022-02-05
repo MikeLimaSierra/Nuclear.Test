c71b815488e06f98626143ffae70cf05756baa84
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-05 12:33:22 +0100

    Create Attributes.cs

---
+++ src/Nuclear.Test.Worker.uTests/Dummies/Attributes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker.Dummies {
+    class DummyAttribute : Attribute { }
+
+    class DummyOnePropAttribute : Attribute {
+
+        public String Name { get; set; }
+
+    }
+
+    class DummyTwoPropsAttribute : Attribute {
+
+        public Int32 Size { get; set; }
+
+        public String Name { get; set; }
+
+    }
+
+    class DummyMixedPropsAttribute : Attribute {
+
+        public Int32 Size { get; set; }
+
+        public String Name { get; set; }
+
+        internal String InternalName { get; set; }
+
+        protected String ProtectedName { get; set; }
+
+        private String PrivateName { get; set; }
+
+    }
+}
