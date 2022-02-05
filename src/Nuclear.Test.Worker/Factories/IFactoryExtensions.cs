e6f5af708978efd045f09d11c4637b97c66f937f
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-05 12:34:07 +0100

    Create IFactoryExtensions.cs

---
+++ src/Nuclear.Test.Worker/Factories/IFactoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Nuclear.Creation;
+
+namespace Nuclear.Test.Worker.Factories {
+
+    /// <summary>
+    /// Defines an extension to <see cref="IFactory"/>.
+    /// </summary>
+    internal static class IFactoryExtensions {
+
+        /// <summary>
+        /// Returns a new instance of <see cref="ICreator{Object, Type}"/>.
+        /// </summary>
+        /// <returns>A  new instance of <see cref="ICreator{Object, Type}"/>.</returns>
+        public static ICreator<Object, Type> TestInstances(this IFactory _) => Factory.Instance.Creator.Create((Type in1) => Create(in1));
+
+        internal static Object Create(Type type) => Activator.CreateInstance(type, true);
+
+    }
+}
