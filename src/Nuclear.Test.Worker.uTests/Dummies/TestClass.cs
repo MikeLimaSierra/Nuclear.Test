f15e2b20c1ea6eab1668f6e92094f175b9edc3a7
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-05 14:59:42 +0100

    log test results for invokation tests into seperate static class

--- src/Nuclear.Test.Worker.uTests/Dummies/TestClass.cs
+++ src/Nuclear.Test.Worker.uTests/Dummies/TestClass.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 using Nuclear.Extensions;
 
 namespace Nuclear.Test.Worker.Dummies {
     internal class TestClass {
-
-        internal StringBuilder ActionResult { get; } = new StringBuilder();
 
         internal static MethodInfo MethodInfo_NoArgs { get; } = typeof(TestClass).GetRuntimeMethods().First(_ => _.Name == nameof(Method_NoArgs));
 
@@ -23,27 +20,27 @@ namespace Nuclear.Test.Worker.Dummies {
         internal static MethodInfo MethodInfo_TwoGeneric_TwoArgs { get; } = typeof(TestClass).GetRuntimeMethods().First(_ => _.Name == nameof(Method_TwoGeneric_TwoArgs));
 
         internal void Method_NoArgs() {
-            ActionResult.AppendLine($"{nameof(MethodInfo_NoArgs)}()");
+            TestResult.ActionResult.AppendLine($"{nameof(MethodInfo_NoArgs)}()");
         }
 
         internal void Method_OneArg(String arg1) {
-            ActionResult.AppendLine($"{nameof(Method_OneArg)}({arg1.Format()})");
+            TestResult.ActionResult.AppendLine($"{nameof(Method_OneArg)}({arg1.Format()})");
         }
 
         internal void Method_TwoArgs(String arg1, Int32 arg2) {
-            ActionResult.AppendLine($"{nameof(Method_TwoArgs)}({arg1.Format()}, {arg2.Format()})");
+            TestResult.ActionResult.AppendLine($"{nameof(Method_TwoArgs)}({arg1.Format()}, {arg2.Format()})");
         }
 
         internal void Method_OneGeneric_NoArgs<T>() {
-            ActionResult.AppendLine($"{nameof(Method_OneGeneric_NoArgs)}<{typeof(T).Format()}>()");
+            TestResult.ActionResult.AppendLine($"{nameof(Method_OneGeneric_NoArgs)}<{typeof(T).Format()}>()");
         }
 
         internal void Method_OneGeneric_OneArg<T>(String arg1) {
-            ActionResult.AppendLine($"{nameof(Method_OneGeneric_OneArg)}<{typeof(T).Format()}>({arg1.Format()})");
+            TestResult.ActionResult.AppendLine($"{nameof(Method_OneGeneric_OneArg)}<{typeof(T).Format()}>({arg1.Format()})");
         }
 
         internal void Method_TwoGeneric_TwoArgs<T1, T2>(String arg1, Int32 arg2) {
-            ActionResult.AppendLine($"{nameof(Method_TwoGeneric_TwoArgs)}<{typeof(T1).Format()}, {typeof(T2).Format()}>({arg1.Format()}, {arg2.Format()})");
+            TestResult.ActionResult.AppendLine($"{nameof(Method_TwoGeneric_TwoArgs)}<{typeof(T1).Format()}, {typeof(T2).Format()}>({arg1.Format()}, {arg2.Format()})");
         }
 
     }
---
+++ src/Nuclear.Test.Worker.uTests/Dummies/TestResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace Nuclear.Test.Worker.Dummies {
+    internal static class TestResult {
+
+        internal static StringBuilder ActionResult { get; } = new StringBuilder();
+
+    }
+}
