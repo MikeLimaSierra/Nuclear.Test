20f117eba4f96bfc354c016b993f03d405ce2952
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-08 21:23:04 +0100

    rename results class

---
+++ src/Nuclear.Test.Worker.uTests/Dummies/TestInvokationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nuclear.Test.Worker.Dummies {
+    internal static class TestInvokationResult {
+
+        internal static List<String> ActionResult { get; } = new List<String>();
+
+        internal static List<Int32> InvokationHashCodes { get; } = new List<Int32>();
+
+        internal static List<Int32> DisposeHashCodes { get; } = new List<Int32>();
+
+    }
+}
--- src/Nuclear.Test.Worker.uTests/Dummies/TestResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-
-namespace Nuclear.Test.Worker.Dummies {
-    internal static class TestResult {
-
-        internal static StringBuilder ActionResult { get; } = new StringBuilder();
-
-    }
-}
