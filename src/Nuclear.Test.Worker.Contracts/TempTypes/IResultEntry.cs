eeb312e9c27bc13a4ef48ecaeb4c9fadb66b3264
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-14 21:22:23 +0100

    Create IResultEntry.cs

---
+++ src/Nuclear.Test.Worker.Contracts/TempTypes/IResultEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker.TempTypes {
+
+    /// <summary>
+    /// Represents a result entry.
+    /// </summary>
+    public interface IResultEntry {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the type of the test entry.
+        /// </summary>
+        EntryTypes EntryType { get; }
+
+        /// <summary>
+        /// Gets the name of the corresponding test instruction.
+        /// </summary>
+        String Instruction { get; }
+
+        /// <summary>
+        /// Gets the message of the corresponding test instruction.
+        /// </summary>
+        String Message { get; }
+
+        #endregion
+
+    }
+
+}
