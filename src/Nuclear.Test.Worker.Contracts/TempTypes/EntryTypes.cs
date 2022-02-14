574ded14b414e2103c91a827ccb00b2a6fb4245e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-14 21:22:05 +0100

    Create EntryTypes.cs

---
+++ src/Nuclear.Test.Worker.Contracts/TempTypes/EntryTypes.cs
@@ -0,0 +1,40 @@
+﻿namespace Nuclear.Test.Worker.TempTypes {
+
+    /// <summary>
+    /// Defines a range of supported entry types.
+    /// </summary>
+    public enum EntryTypes {
+
+        /// <summary>
+        /// The entry is a note.
+        /// </summary>
+        Note,
+
+        /// <summary>
+        /// The entry marks the injection of data from a data source.
+        /// </summary>
+        DataSource,
+
+        /// <summary>
+        /// The entry is a test method data injection.
+        /// </summary>
+        Injection,
+
+        /// <summary>
+        /// The entry is an error.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// The entry is a positive test result.
+        /// </summary>
+        ResultOk,
+
+        /// <summary>
+        /// The entry is a negative test result.
+        /// </summary>
+        ResultFail,
+
+    }
+
+}
