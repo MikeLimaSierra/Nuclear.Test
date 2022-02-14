ad38dc8bf002f2315997b45f20784df1ae1986c1
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-14 22:34:31 +0100

    Create ResultEntry.cs

---
+++ src/Nuclear.Test.Worker/TempTypes/ResultEntry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Nuclear.Exceptions;
+
+namespace Nuclear.Test.Worker.TempTypes {
+    internal class ResultEntry : IResultEntry {
+
+        #region properties
+
+        public EntryTypes EntryType { get; }
+        
+        public String Instruction { get; }
+        
+        public String Message { get; }
+
+        #endregion
+
+        #region ctors
+
+        public ResultEntry(EntryTypes type, String instruction, String message) {
+            Throw.IfNot.Enum.IsDefined<EntryTypes>(type, nameof(type));
+            Throw.If.String.IsNullOrWhiteSpace(message, nameof(message));
+
+            EntryType = type;
+            Message = message;
+
+            if(EntryType == EntryTypes.ResultOk || EntryType == EntryTypes.ResultFail) {
+                Throw.If.String.IsNullOrWhiteSpace(instruction, nameof(instruction));
+               
+                Instruction = instruction;
+            }            
+        }
+
+        #endregion
+
+        #region methods
+
+        #endregion
+
+    }
+
+}
