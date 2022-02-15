47ccdfce55716df471d899762be9f755fedf12d1
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:02:07 +0100

    Create ITestMethodResults.cs

---
+++ src/Nuclear.Test.Worker.Contracts/TempTypes/ITestMethodResults.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker.TempTypes {
+
+    /// <summary>
+    /// Represents the results of a number of executed test instructions (e.g. executed test method).
+    /// </summary>
+    public interface ITestMethodResults {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the result entries registered to the test methods.
+        /// </summary>
+        IResultEntryCollection Entries { get; }
+
+        /// <summary>
+        /// Gets the reason why the test method is being ignored.
+        /// </summary>
+        String IgnoreReason { get; }
+
+        /// <summary>
+        /// Gets if the test method was ignored.
+        /// </summary>
+        Boolean IsIgnored { get; }
+
+        #endregion
+
+    }
+
+}
