2588a356f394c2128649c4e770aabaef594155ea
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:02:18 +0100

    Create TestMethodResults_uTests.cs

---
+++ src/Nuclear.Test.Worker.uTests/TempTypes/TestMethodResults_uTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Nuclear.TestSite;
+
+using TestX = Nuclear.TestSite.Test;
+
+namespace Nuclear.Test.Worker.TempTypes {
+    class TestMethodResults_uTests {
+
+        [TestMethod]
+        void Implementation() {
+
+            TestX.If.Type.Implements<TestMethodResults, ITestMethodResults>();
+
+        }
+
+        #region Ctor
+
+        [TestMethod]
+        [TestData(nameof(Ctor_Data))]
+        void Ctor(String in1, (String reason, Boolean isIgnored) expected) {
+
+            ITestMethodResults sut = default;
+
+            TestX.IfNot.Action.ThrowsException(() => sut = new TestMethodResults(in1), out Exception _);
+
+            TestX.If.Enumerable.IsEmpty(sut.Entries);
+            TestX.If.Value.IsEqual(sut.IgnoreReason, expected.reason);
+            TestX.If.Value.IsEqual(sut.IsIgnored, expected.isIgnored);
+
+        }
+
+        IEnumerable<Object[]> Ctor_Data() {
+            yield return new Object[] { null, ((String) null, false) };
+            yield return new Object[] { "", ("", false) };
+            yield return new Object[] { " ", (" ", false) };
+            yield return new Object[] { "a", ("a", true) };
+        }
+
+        #endregion
+
+    }
+}
