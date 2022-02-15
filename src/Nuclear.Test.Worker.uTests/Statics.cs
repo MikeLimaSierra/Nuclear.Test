273bce8a99250c633f916fabbf06e09b45c33e7e
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:03:10 +0100

    Create Statics.cs

---
+++ src/Nuclear.Test.Worker.uTests/Statics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using Nuclear.Extensions;
+using Nuclear.Test.Worker.TempTypes;
+
+namespace Nuclear.Test.Worker {
+    internal static class Statics {
+
+        internal static IEqualityComparer<IResultEntry> ResultEntryComparer { get; }
+
+        static Statics() {
+            ResultEntryComparer = DynamicEqualityComparer.FromDelegate<IResultEntry>(
+                (x, y) => x.EntryType == y.EntryType && x.Instruction == y.Instruction && x.Message == y.Message,
+                (obj) => obj.GetHashCode()
+            );
+        }
+
+    }
+}
