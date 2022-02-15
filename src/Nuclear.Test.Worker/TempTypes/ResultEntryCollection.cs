29d1f098d370ca3bf85882cc248cf540d6699fe0
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:01:53 +0100

    Create ResultEntryCollection.cs

---
+++ src/Nuclear.Test.Worker/TempTypes/ResultEntryCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nuclear.Test.Worker.TempTypes {
+    internal class ResultEntryCollection : List<IResultEntry>, IResultEntryCollection {
+
+        #region properties
+
+        public Int32 CountRelevantEntries => CountResults + CountErrors;
+
+        public Int32 CountResults => CountResultsOk + CountResultsFailed;
+
+        public Int32 CountResultsOk => this.Where(_ => _.EntryType == EntryTypes.ResultOk).Count();
+
+        public Int32 CountResultsFailed => this.Where(_ => _.EntryType == EntryTypes.ResultFail).Count();
+
+        public Int32 CountErrors => this.Where(_ => _.EntryType == EntryTypes.Error).Count();
+
+        public Boolean IsFailed => CountResultsFailed > 0 || CountErrors > 0;
+
+        public Boolean IsEmpty => Count == 0;
+
+        #endregion
+
+        #region ctors
+
+        internal ResultEntryCollection() : base() { }
+
+        internal ResultEntryCollection(IEnumerable<IResultEntry> collection) : base(collection) { }
+
+        #endregion
+
+    }
+}
