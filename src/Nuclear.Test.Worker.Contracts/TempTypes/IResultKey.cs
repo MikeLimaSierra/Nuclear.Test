df1be5464bb2f2f3c2f7d32512b0f84b37f67567
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:02:50 +0100

    add result key and scenario

---
+++ src/Nuclear.Test.Worker.Contracts/TempTypes/IResultKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker.TempTypes {
+
+    /// <summary>
+    /// Defines the key structure that identifies test method results.
+    /// </summary>
+    public interface IResultKey : IEquatable<IResultKey>, IComparable<IResultKey> {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the test scenario.
+        /// </summary>
+        IScenario Scenario { get; }
+
+        /// <summary>
+        /// Gets the file name of the test.
+        /// </summary>
+        String FileName { get; }
+
+        /// <summary>
+        /// Gets the calling method name of the test.
+        /// </summary>
+        String MethodName { get; }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Nuclear.Test.Worker.Contracts/TempTypes/IScenario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+using Nuclear.Assemblies.Runtimes;
+
+namespace Nuclear.Test.Worker.TempTypes {
+
+    /// <summary>
+    /// Defines a scenario relevant to running tests.
+    /// </summary>
+    public interface IScenario : IEquatable<IScenario>, IComparable<IScenario> {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the name of the test assembly.
+        /// </summary>
+        String AssemblyName { get; }
+
+        /// <summary>
+        /// Gets or sets the targeted runtime version.
+        /// </summary>
+        RuntimeInfo TargetRuntime { get; }
+
+        /// <summary>
+        /// Gets the targeted processor architecture.
+        /// </summary>
+        ProcessorArchitecture TargetArchitecture { get; }
+
+        /// <summary>
+        /// Gets or sets the executing runtime version.
+        /// </summary>
+        RuntimeInfo ExecutionRuntime { get; }
+
+        /// <summary>
+        /// Gets the executing processor architecture.
+        /// </summary>
+        ProcessorArchitecture ExecutionArchitecture { get; }
+
+        #endregion
+
+    }
+
+}
