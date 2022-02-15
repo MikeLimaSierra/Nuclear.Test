6f8a0ddbb22bece38fd65556e9c46f14b96f8920
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:03:33 +0100

    Update TempTypes.cs

--- src/Nuclear.Test.Worker/TempTypes.cs
+++ src/Nuclear.Test.Worker/TempTypes.cs
@@ -1,29 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 
-using Nuclear.Assemblies.Runtimes;
 using Nuclear.Test.Worker.TempTypes;
 
 namespace Nuclear.Test.Worker {
-
-
-
-    public interface ITestMethodResults : IResultEntryCollection {
-        String IgnoreReason { get; }
-        void Ignore(String message);
-    }
-
-    public interface IResultKey : IEquatable<IResultKey>, IComparable<IResultKey> {
-        String AssemblyName { get; }
-        RuntimeInfo TargetRuntime { get; }
-        ProcessorArchitecture TargetArchitecture { get; }
-        RuntimeInfo ExecutionRuntime { get; }
-        ProcessorArchitecture ExecutionArchitecture { get; }
-        String FileName { get; }
-        String MethodName { get; }
-    }
-
     public interface IResultCollection : IEnumerable<KeyValuePair<IResultKey, IResultEntry>> {
         void Add(IResultKey key, ITestMethodResults results);
     }
