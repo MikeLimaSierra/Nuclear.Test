3572ed16016d91bf18b2d574c199f7d51d4a6e7f
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-15 21:02:22 +0100

    Create TestMethodResults.cs

---
+++ src/Nuclear.Test.Worker/TempTypes/TestMethodResults.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker.TempTypes {
+    internal class TestMethodResults : ITestMethodResults {
+
+        #region properties
+
+        public IResultEntryCollection Entries { get; } = new ResultEntryCollection();
+
+        public String IgnoreReason { get; internal set; }
+
+        public Boolean IsIgnored => !String.IsNullOrWhiteSpace(IgnoreReason);
+
+        #endregion
+
+        #region ctors
+
+        internal TestMethodResults(String ignoreReason) {
+            IgnoreReason = ignoreReason;
+        }
+
+        #endregion
+
+    }
+}
