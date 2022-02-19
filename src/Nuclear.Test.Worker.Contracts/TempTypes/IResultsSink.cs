a07b8afdad0d642ef233e33877c09d2e8ea4d9f2
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-19 12:57:12 +0100

    Create IResultsSink.cs

---
+++ src/Nuclear.Test.Worker.Contracts/TempTypes/IResultsSink.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nuclear.Test.Worker.TempTypes {
+    public interface IResultsSink {
+        void AddNote(String message, String _file, String _method);
+        void AddResult(Boolean result, String testInstruction, String message, String _file, String _method);
+    }
+}
