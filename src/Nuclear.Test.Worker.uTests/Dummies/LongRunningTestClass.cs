b9aedee90c6c5c13ec0a4cad737631b9938dfb00
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2022-02-19 12:58:01 +0100

    add longer running test classes for time logging during invokations

---
+++ src/Nuclear.Test.Worker.uTests/Dummies/LongRunningTestClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Nuclear.Test.Worker.Dummies {
+    internal class LongRunningTestClass : IDisposable {
+
+        internal static MethodInfo MethodInfo_Do { get; } = typeof(LongRunningTestClass).GetRuntimeMethods().First(_ => _.Name == nameof(Do));
+
+        #region fields
+
+        private TimeSpan _constructTime = TimeSpan.FromMilliseconds(1000);
+
+        private TimeSpan _invokeTime = TimeSpan.FromMilliseconds(2000);
+
+        private TimeSpan _disposeTime = TimeSpan.FromMilliseconds(3000);
+
+        #endregion
+
+        #region ctor
+
+        internal LongRunningTestClass() {
+            Thread.Sleep(_constructTime);
+        }
+
+        #endregion
+
+        #region methods
+
+        internal void Do() {
+            Thread.Sleep(_invokeTime);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        private Boolean _disposedValue;
+
+        protected virtual void Dispose(Boolean disposing) {
+            if(!_disposedValue) {
+                if(disposing) {
+                    // TODO: dispose managed state (managed objects)
+                    Thread.Sleep(_disposeTime);
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Test.Worker.uTests/Dummies/ShortRunningTestClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace Nuclear.Test.Worker.Dummies {
+    internal class ShortRunningTestClass : IDisposable {
+
+        internal static MethodInfo MethodInfo_Do { get; } = typeof(ShortRunningTestClass).GetRuntimeMethods().First(_ => _.Name == nameof(Do));
+
+        #region fields
+
+        private TimeSpan _constructTime = TimeSpan.FromMilliseconds(500);
+
+        private TimeSpan _invokeTime = TimeSpan.FromMilliseconds(1000);
+
+        private TimeSpan _disposeTime = TimeSpan.FromMilliseconds(1500);
+
+        #endregion
+
+        #region ctor
+
+        internal ShortRunningTestClass() {
+            Thread.Sleep(_constructTime);
+        }
+
+        #endregion
+
+        #region methods
+
+        internal void Do() {
+            Thread.Sleep(_invokeTime);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        private Boolean _disposedValue;
+
+        protected virtual void Dispose(Boolean disposing) {
+            if(!_disposedValue) {
+                if(disposing) {
+                    // TODO: dispose managed state (managed objects)
+                    Thread.Sleep(_disposeTime);
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+}
